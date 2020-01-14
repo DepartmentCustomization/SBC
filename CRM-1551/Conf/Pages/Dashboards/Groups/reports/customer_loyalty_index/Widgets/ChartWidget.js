95d34950d30bf7992fdd05061dd85e45b3fb9c19
Yehor <egorkatornado@gmail.com>
2020-01-14 12:22:03 +0200

    Task 1551_624 is done

--- CRM-1551/Conf/Pages/Dashboards/Groups/reports/customer_loyalty_index/Widgets/ChartWidget.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/reports/customer_loyalty_index/Widgets/ChartWidget.js
@@ -1,60 +1,128 @@
 (function () {
     return {
         chartConfig: {
+            chart: {
+                type: 'line'
+            },
             title: {
                 text: 'Загальне число'
-            },       
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: {
+                categories: []
+            },
             yAxis: {
                 title: {
-                    text: ' '
+                    text: 'Динаміка'
                 }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
             },
             plotOptions: {
-                series: {
-                    label: {
-                        connectorAllowed: false
+                line: {
+                    dataLabels: {
+                        enabled: true
                     },
-                    pointStart: 1
+                    enableMouseTracking: false
                 }
             },
-            series: [{
-                name: 'Installation',
-                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-            }, {
-                name: 'Manufacturing',
-                data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-            }, {
-                name: 'Sales & Distribution',
-                data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-            }, {
-                name: 'Project Development',
-                data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-            }, {
-                name: 'Other',
-                data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-            }],
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
-                    },
-                    chartOptions: {
-                        legend: {
-                            layout: 'horizontal',
-                            align: 'center',
-                            verticalAlign: 'bottom'
-                        }
-                    }
-                }]
-            }
+            series: []
         },
         init: function() {
+            const query = this.query("ak_NPS_Index0_0");
+            this.queryExecutor(query, this.getYesterdayIndex, this);
+            this.showPreloader = false;
+            this.sub = this.messageService.subscribe('GlobalFilterChanged', this.getFiltersParams, this);
+        },
+        getYesterdayIndex: function (data) {
+            let value = 0;
+            if (data.rows.length) {
+                value = data.rows[0].values[0];
+            }
+            this.chartConfig.subtitle.text =  value;
+        },
+        getFiltersParams: function (message) {
+            let period = message.package.value.values.find((el) => {
+                return el.name.toLowerCase() === 'period';
+            });
+            const value = period.value;
+            if (value !== null) {
+                if (value.dateFrom !== '' && value.dateTo !== '') {
+                    this.dateFrom = value.dateFrom;
+                    this.dateTo = value.dateTo;
+                    this.executeQuery();
+                }
+            }
+        },
+        executeQuery: function () {
+            const query = this.query("ak_NPS_Graf0_0");
+            this.queryExecutor(query, this.load, this);
+            this.showPreloader = false;
+        },
+        query: function (queryCode) {
+            return {
+                "queryCode": queryCode,
+                "limit": -1,
+                "parameterValues": [
+                    { "key": "@date_from", "value": this.dateFrom },
+                    { "key": "@date_to", "value": this.dateTo }
+                ],
+            };
+        },
+        load: function (data) {
+            this.fillIndexes(data);
+            this.setChartSeries(data);
             this.render();
         },
+        fillIndexes: function (data) {
+            this.id = this.getIndex(data, 'id');
+            this.integralIndicator = this.getIndex(data, 'integral_indicator');
+            this.calcDate = this.getIndex(data, 'calc_date');
+        },
+        getIndex: function (data, name) {
+            return data.columns.findIndex(el => {
+                return el.code.toLowerCase() === name;
+            })
+        },
+        setChartSeries: function (data) {
+            const chartData = {
+                name: 'Дні',
+                data: this.getSeriesData(data)
+            };
+            const categories = this.getCategories(data);
+            this.chartConfig.series = [];
+            this.chartConfig.series.push(chartData);
+            this.chartConfig.xAxis.categories = [];
+            this.chartConfig.xAxis.categories = categories;
+        },
+        getSeriesData: function (data) {
+            let result = [];
+            for (let i = 0; i < data.rows.length; i++) {
+                let value = data.rows[i].values[this.integralIndicator];
+                value = value === null ? 0 : value;
+                result.push(value);
+            }
+            return result;
+        },
+        getCategories: function (data) {
+            let result = [];
+            for (let i = 0; i < data.rows.length; i++) {
+                let value = this.setDete(data.rows[i].values[this.calcDate]);
+                result.push(value);
+            }
+            return result;
+        },
+        setDete: function (value) {
+            let date = new Date(value);
+            let dd = date.getDate().toString();
+            let mm = (date.getMonth() + 1).toString();
+            let yy = date.getFullYear().toString().slice(-2);
+            dd = dd.length === 1 ? '0' + dd : dd;
+            mm = mm.length === 1 ? '0' + mm : mm;
+            return  dd + '-' + mm + '-' + yy;  
+        },
+        destroy: function () {
+            this.sub.unsubscribe();
+        }
     };
 }());
