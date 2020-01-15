9754f505f46a0346fad4e9c3cc0b74f6e8513910
Виталий Носков <v.noskov@domain.local>
2020-01-15 14:37:39 +0200

    Исправил ошибки ESLint в форме ввода Form_New_Applicant

--- CRM-1551/Conf/Forms/Form_New_Applicant/Form_New_Applicant.form.js
+++ CRM-1551/Conf/Forms/Form_New_Applicant/Form_New_Applicant.form.js
@@ -3,10 +3,9 @@
     init: function(){
         this.form.disableControl('phone_number');
     },
-    afterSave: function(data){
+    afterSave: function(){
         const appeal_id = this.form.getControlValue('appeals');
-        this.navigateTo('/sections/Appeals/edit/'+appeal_id);
-      // back();
+        this.navigateTo('/sections/Appeals/edit/' + appeal_id);
     }
 };
 }());
