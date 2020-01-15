3e7cc435d3b91e5fda86c5932e9e5486e13b646c
Виталий Носков <v.noskov@domain.local>
2020-01-15 14:00:49 +0200

    Исправил ошибки ESLint в форме ввода Hotline

--- CRM-1551/Conf/Forms/Hotline/Hotline.form.js
+++ CRM-1551/Conf/Forms/Hotline/Hotline.form.js
@@ -1,15 +1,12 @@
 (function() {
     return {
         init: function() {
-                        this.form.disableControl('Id');
-                        this.form.disableControl('registration_date');
-                        this.form.disableControl('phone_number');
-                        //this.form.setControlValue('Id', this.id);
-                        //this.form.setControlValue('Appeal_Id', this.id);
+            this.form.disableControl('Id');
+            this.form.disableControl('registration_date');
+            this.form.disableControl('phone_number');
         },
         afterSave: function(data){
-            // const id = this.form.getControlValue('Id');
-             this.navigateTo('/sections/Hotline/edit/'+data.rows[0].values[0]);
+             this.navigateTo('/sections/Hotline/edit/' + data.rows[0].values[0]);
          } 
     }
     }());
