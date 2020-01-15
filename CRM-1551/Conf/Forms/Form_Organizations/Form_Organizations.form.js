687f21c824d3d556516b9ec38f8793156f36484f
Виталий Носков <v.noskov@domain.local>
2020-01-15 14:36:32 +0200

    Исправил ошибки ESLint в форме ввода Form_Organizations

--- CRM-1551/Conf/Forms/Form_Organizations/Form_Organizations.form.js
+++ CRM-1551/Conf/Forms/Form_Organizations/Form_Organizations.form.js
@@ -7,13 +7,14 @@
         this.form.disableControl('');
         let dependParams = [{ parameterCode: '@organization_id', parameterValue: this.id }];
         this.form.setControlParameterValues('organization_id', dependParams);
-   let icon = document.getElementById('phone_numberIcon');
-    document.getElementById('phone_numberIcon').style.fontSize = '35px';
-    icon.addEventListener('click', function() {
-                        let xhr = new XMLHttpRequest();
-                        xhr.open('GET', `http://172.16.0.197:5566/CallService/Call/number=`+phone_number.value+`&operator=699`);
-                xhr.send();
-              });
+        let icon = document.getElementById('phone_numberIcon');
+        document.getElementById('phone_numberIcon').style.fontSize = '35px';
+        icon.addEventListener('click', function() {
+          let phone_number = document.getElementById('phone_number');
+          let xhr = new XMLHttpRequest();
+          xhr.open('GET', `http://172.16.0.197:5566/CallService/Call/number=` + phone_number.value + `&operator=699`);
+          xhr.send();
+        });
     }
 };
 }());
