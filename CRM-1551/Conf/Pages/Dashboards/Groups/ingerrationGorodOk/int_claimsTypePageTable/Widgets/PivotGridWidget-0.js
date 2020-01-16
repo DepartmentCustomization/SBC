06aa645a875ff7bcc1ed72ff5c0109cdcdb6c774
Yehor <egorkatornado@gmail.com>
2020-01-16 13:29:21 +0200

    Update DBs

--- CRM-1551/Conf/Pages/Dashboards/Groups/ingerrationGorodOk/int_claimsTypePageTable/Widgets/PivotGridWidget-0.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/ingerrationGorodOk/int_claimsTypePageTable/Widgets/PivotGridWidget-0.js
@@ -1,140 +1,136 @@
 (function () {
-  return {
-    config: {
-        query: {
-            code: 'int_claims_typePageTable',
-            parameterValues: [],
-            filterColumns: [],
-            sortColumns: [],
-            skipNotVisibleColumns: true,
-            chunkSize: 1000
-        },
-        columns:[
-            {
-                dataField: 'operation',
-                caption: 'Операція',
-                width: 100
-            },{
-              caption: 'ГородОк',
-              alignment: 'center',
-              columns: [
-                        {
-                        dataField: 'become',
-                        caption: 'Було',
-                        alignment: 'left',
-                    },{
-                        dataField: 'it_was',
-                        caption: 'Стало',
-                        alignment: 'left',
-                    }
-            ]  
+    return {
+        config: {
+            query: {
+                code: 'int_claims_typePageTable',
+                parameterValues: [],
+                filterColumns: [],
+                sortColumns: [],
+                skipNotVisibleColumns: true,
+                chunkSize: 1000
             },
-            {
-                dataField: 'is_done',
-                caption: 'Стан',
-                width: 80
-            },{
-                dataField: 'comment',
-                caption: 'Коментар'
-            }
-        ],  
-        searchPanel: {
-            visible: true,
-            highlightCaseSensitive: false
+            columns:[
+                {
+                    dataField: 'operation',
+                    caption: 'Операція',
+                    width: 100
+                },{
+                caption: 'ГородОк',
+                alignment: 'center',
+                columns: [
+                            {
+                            dataField: 'become',
+                            caption: 'Було',
+                            alignment: 'left',
+                        },{
+                            dataField: 'it_was',
+                            caption: 'Стало',
+                            alignment: 'left',
+                        }
+                ]  
+                },
+                {
+                    dataField: 'is_done',
+                    caption: 'Стан',
+                    width: 80
+                },{
+                    dataField: 'comment',
+                    caption: 'Коментар'
+                }
+            ],  
+            searchPanel: {
+                visible: true,
+                highlightCaseSensitive: false
+            },
+            pager: {
+                showPageSizeSelector:  false,
+                allowedPageSizes: [10, 15, 30],
+                showInfo: true,
+                pageSize: 10
+            },
+            export: {
+                enabled: true,
+                fileName: 'File_name'
+            },
+            editing: {
+                mode: 'cell',
+                allowUpdating: true,
+                useIcons: true
+            },
+            filterRow: {
+                visible: false,
+                applyFilter: "auto"
+            },
+            keyExpr: 'Id',
+            showBorders: true,
+            showColumnLines: true,
+            showRowLines: true,
+            remoteOperations: null,
+            allowColumnReordering: null,
+            rowAlternationEnabled: null,
+            columnAutoWidth: null,
+            hoverStateEnabled: true,
+            columnWidth: null,
+            wordWrapEnabled: true,
+            allowColumnResizing: true,
+            showFilterRow: false,
+            showHeaderFilter: false,
+            showColumnChooser: true,
+            showColumnFixing: true,
+            groupingAutoExpandAll: null
         },
-        pager: {
-            showPageSizeSelector:  false,
-            allowedPageSizes: [10, 15, 30],
-            showInfo: true,
-            pageSize: 10
-        },
-        export: {
-            enabled: true,
-            fileName: 'File_name'
-        },
-        editing: {
-            mode: 'batch',
-            allowUpdating: true,
-            useIcons: true
-        },
-        filterRow: {
-            visible: false,
-            applyFilter: "auto"
-        },
-        height: '550',
-        keyExpr: 'Id',
-        showBorders: true,
-        showColumnLines: true,
-        showRowLines: true,
-        remoteOperations: null,
-        allowColumnReordering: null,
-        rowAlternationEnabled: null,
-        columnAutoWidth: null,
-        hoverStateEnabled: true,
-        columnWidth: null,
-        wordWrapEnabled: true,
-        allowColumnResizing: true,
-        showFilterRow: false,
-        showHeaderFilter: false,
-        showColumnChooser: true,
-        showColumnFixing: true,
-        groupingAutoExpandAll: null
-    },
-    elements: [],
-    init: function() {
-        this.loadData(this.afterLoadDataHandler);
-        this.dataGridInstance.onRowUpdating.subscribe( function(e) {
-            let is_done = e.newData.is_done;
-            let key = e.key;
-            let comment = e.newData.comment;
-            let cat_id = e.oldData.cat_id;
-            let saveChange = {
-                queryCode: 'int_btnSaveChange_claims_typeGorodok',
-                limit: -1,
-                parameterValues: [
-                    {
-                        key: '@key',
-                        value: key
-                    },{
-                        key: '@is_done',
-                        value: is_done
-                    },{
-                        key: '@comment',
-                        value: comment
-                    },{
-                        key: '@cat_id',
-                        value: cat_id
-                    }
-                ]
-            };
-            this.queryExecutor(saveChange);
-        }.bind(this));
-    },
-    afterLoadDataHandler: function() {
-        this.render();
-    },
-    createElement: function(tag, props, ...children) {
-        const element = document.createElement(tag);
-        Object.keys(props).forEach( key => element[key] = props[key] );
-        if(children.length > 0){
-            children.forEach( child =>{
-                element.appendChild(child);
+        elements: [],
+        init: function() {
+            this.dataGridInstance.height = window.innerHeight - 100;
+            this.config.onContentReady = this.afterRenderTable.bind(this);
+            this.loadData(this.afterLoadDataHandler);
+            this.dataGridInstance.onRowUpdating.subscribe( e => {
+                let is_done = e.newData.is_done;
+                let key = e.key;
+                let comment = e.newData.comment;
+                let cat_id = e.oldData.cat_id;
+                let saveChange = {
+                    queryCode: 'int_btnSaveChange_claims_typeGorodok',
+                    limit: -1,
+                    parameterValues: [
+                        {
+                            key: '@key',
+                            value: key
+                        },{
+                            key: '@is_done',
+                            value: is_done
+                        },{
+                            key: '@comment',
+                            value: comment
+                        },{
+                            key: '@cat_id',
+                            value: cat_id
+                        }
+                    ]
+                };
+                this.queryExecutor(saveChange);
             });
-        } return element;
-    },       
-    createCustomStyle:function(){
-        let element = document.querySelector('.dx-datagrid-save-button');
-        element.style.marginRight = '9px';
-        element.style.backgroundColor = '#5cb85c';
-        element.firstElementChild.firstElementChild.style.color = '#fff';
-        element.firstElementChild.lastElementChild.style.color = '#fff';
-        element.parentElement.parentElement.classList.remove('dx-toolbar-text-auto-hide');
-    },
-    subscribeToDataGridActions: function() {
-    },
-    onDataGridEditorPreparing: function() {
-    },
-    destroy: function() {
-    } 
-};
+        },
+        afterRenderTable: function() {
+            const btnExcel = document.querySelector('.dx-datagrid-export-button');
+            const spanElement = this.createElement('span', { className: 'dx-button-text', innerText: 'Excel'});
+            if(btnExcel.firstElementChild.childNodes.length === 1) {
+                btnExcel.firstElementChild.appendChild(spanElement);
+            }
+        },
+        afterLoadDataHandler: function() {
+            this.render();
+        },
+        createElement: function(tag, props, ...children) {
+            const element = document.createElement(tag);
+            Object.keys(props).forEach( key => element[key] = props[key] );
+            if(children.length > 0){
+                children.forEach( child =>{
+                    element.appendChild(child);
+                });
+            } return element;
+        },
+        destroy: function() {
+        }
+    };
 }());
--- CRM-1551/Conf/Pages/Dashboards/Groups/ingerrationGorodOk/int_streetsPageTable/Widgets/PivotGridWidget-0.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/ingerrationGorodOk/int_streetsPageTable/Widgets/PivotGridWidget-0.js
@@ -1,159 +1,176 @@
 (function () {
-  return {
-    config: {
-        query: {
-            code: 'int_streetsPageTable',
-            parameterValues: [],
-            filterColumns: [],
-            sortColumns: [],
-            skipNotVisibleColumns: true,
-            chunkSize: 1000
-        },
-        columns:[
-            {
-                dataField: 'operation',
-                caption: 'Операція',
-                width: 100
-            },{
-              caption: 'ГородОк',
-              alignment: 'center',
-              columns: [
-                        {
-                        dataField: 'become',
-                        caption: 'Було',
-                        alignment: 'left',
-                    },{
-                        dataField: 'it_was',
-                        caption: 'Стало',
-                        alignment: 'left',
-                    }
-            ]  
-            },{
-                dataField: 'id_1551',
-                caption: 'Назва вулиці у системі 1551',
-                lookup: {
-                    dataSource: {
-                        paginate: true,
-                        store: this.elements
-                    },
-                    displayExpr: "streets",
-                    valueExpr: "Id"
-                }
-            },{
-                dataField: 'is_done',
-                caption: 'Стан',
-                width: 80
-            },{
-                dataField: 'comment',
-                caption: 'Коментар'
-            }
-        ], searchPanel: {
-            visible: true,
-            highlightCaseSensitive: false
-        },
-        pager: {
-            showPageSizeSelector:  false,
-            allowedPageSizes: [10, 15, 30],
-            showInfo: true,
-            pageSize: 10
-        },
-        export: {
-            enabled: true,
-            fileName: 'File_name'
-        },
-        editing: {
-            mode: 'batch',
-            allowUpdating: true,
-            useIcons: true
-        },
-        filterRow: {
-            visible: false,
-            applyFilter: "auto"
-        },
-        height: '750',
-        keyExpr: 'Id',
-        showBorders: true,
-        showColumnLines: true,
-        showRowLines: true,
-        remoteOperations: null,
-        allowColumnReordering: null,
-        rowAlternationEnabled: null,
-        columnAutoWidth: null,
-        hoverStateEnabled: true,
-        columnWidth: null,
-        wordWrapEnabled: true,
-        allowColumnResizing: true,
-        showFilterRow: false,
-        showHeaderFilter: false,
-        showColumnChooser: true,
-        showColumnFixing: true,
-        groupingAutoExpandAll: null
-    },
-    elements: [],
-    init: function() {
-        this.loadData(this.afterLoadDataHandler);
-        let executeQuery = {
-                queryCode: 'int_list_streets_1551',
+    return {
+        config: {
+            query: {
+                code: 'int_streetsPageTable',
                 parameterValues: [],
-                limit: -1
-            };
-            this.queryExecutor(executeQuery, this.lookupFoo, this);
-        this.dataGridInstance.onRowUpdating.subscribe( function(e) {
-            let is_done = e.newData.is_done;
-            let key = e.key;
-            let id_1551 = e.oldData.id_1551;
-            let id_1551_new = e.newData.id_1551;
-            let comment = e.newData.comment;
-            let cat_id = e.oldData.cat_id;
-            let saveChange = {
-                queryCode: 'int_btnSaveChange_streetGorodok',
-                limit: -1,
-                parameterValues: [
-                    {
-                        key: '@key',
-                        value: key
-                    },{
-                        key: '@is_done',
-                        value: is_done
-                    },{
-                        key: '@id_1551',
-                        value: id_1551
-                    },{
-                        key: '@id_1551_new',
-                        value: id_1551_new
-                    },{
-                        key: '@comment',
-                        value: comment
-                    },{
-                        key: '@cat_id',
-                        value: cat_id
+                filterColumns: [],
+                sortColumns: [],
+                skipNotVisibleColumns: true,
+                chunkSize: 1000
+            },
+            columns:[
+                {
+                    dataField: 'operation',
+                    caption: 'Операція',
+                    width: 100
+                },{
+                caption: 'ГородОк',
+                alignment: 'center',
+                columns: [
+                            {
+                            dataField: 'become',
+                            caption: 'Було',
+                            alignment: 'left',
+                        },{
+                            dataField: 'it_was',
+                            caption: 'Стало',
+                            alignment: 'left',
+                        }
+                ]  
+                },{
+                    dataField: 'id_1551',
+                    caption: 'Назва вулиці у системі 1551',
+                    lookup: {
+                        dataSource: {
+                            paginate: true,
+                            store: this.elements
+                        },
+                        displayExpr: "streets",
+                        valueExpr: "Id"
                     }
-                ]
-            };
-            this.queryExecutor(saveChange);
-        }.bind(this));
-    },
-     lookupFoo: function(data) {
-        this.elements = [];
-        for(let i = 0; i < data.rows.length; i++){
-            let el = data.rows[i];
-            let obj = {
-                "Id": el.values[0],
-                "streets": el.values[1],
-            } 
-            this.elements.push(obj);
+                },{
+                    dataField: 'is_done',
+                    caption: 'Стан',
+                    width: 80
+                },{
+                    dataField: 'comment',
+                    caption: 'Коментар'
+                }
+            ], searchPanel: {
+                visible: true,
+                highlightCaseSensitive: false
+            },
+            pager: {
+                showPageSizeSelector:  false,
+                allowedPageSizes: [10, 15, 30],
+                showInfo: true,
+                pageSize: 10
+            },
+            export: {
+                enabled: true,
+                fileName: 'File_name'
+            },
+            editing: {
+                mode: 'cell',
+                allowUpdating: true,
+                useIcons: true
+            },
+            filterRow: {
+                visible: false,
+                applyFilter: "auto"
+            },
+            keyExpr: 'Id',
+            showBorders: true,
+            showColumnLines: true,
+            showRowLines: true,
+            remoteOperations: null,
+            allowColumnReordering: null,
+            rowAlternationEnabled: null,
+            columnAutoWidth: null,
+            hoverStateEnabled: true,
+            columnWidth: null,
+            wordWrapEnabled: true,
+            allowColumnResizing: true,
+            showFilterRow: false,
+            showHeaderFilter: false,
+            showColumnChooser: true,
+            showColumnFixing: true,
+            groupingAutoExpandAll: null
+        },
+        elements: [],
+        init: function() {
+            this.dataGridInstance.height = window.innerHeight - 100;
+            this.config.onContentReady = this.afterRenderTable.bind(this);
+            this.loadData(this.afterLoadDataHandler);
+            let executeQuery = {
+                    queryCode: 'int_list_streets_1551',
+                    parameterValues: [],
+                    limit: -1
+                };
+                this.queryExecutor(executeQuery, this.lookupFoo, this);
+                this.dataGridInstance.onRowUpdating.subscribe( function(e) {
+                let is_done = e.newData.is_done;
+                let key = e.key;
+                let id_1551 = e.oldData.id_1551;
+                let id_1551_new = e.newData.id_1551;
+                let comment = e.newData.comment;
+                let cat_id = e.oldData.cat_id;
+                let saveChange = {
+                    queryCode: 'int_btnSaveChange_streetGorodok',
+                    limit: -1,
+                    parameterValues: [
+                        {
+                            key: '@key',
+                            value: key
+                        },{
+                            key: '@is_done',
+                            value: is_done
+                        },{
+                            key: '@id_1551',
+                            value: id_1551
+                        },{
+                            key: '@id_1551_new',
+                            value: id_1551_new
+                        },{
+                            key: '@comment',
+                            value: comment
+                        },{
+                            key: '@cat_id',
+                            value: cat_id
+                        }
+                    ]
+                };
+                this.queryExecutor(saveChange);
+            }.bind(this));
+        },
+        lookupFoo: function(data) {
+            this.elements = [];
+            for(let i = 0; i < data.rows.length; i++){
+                let el = data.rows[i];
+                let obj = {
+                    "Id": el.values[0],
+                    "streets": el.values[1],
+                } 
+                this.elements.push(obj);
+            }
+            this.config.columns[2].lookup.dataSource.store = this.elements;
+            this.loadData(this.afterLoadDataHandler);
+        },
+        afterRenderTable: function(){
+            const btnExcel = document.querySelector('.dx-datagrid-export-button');
+            const spanElement = this.createElement('span', { className: 'dx-button-text', innerText: 'Excel'});
+            if(btnExcel.firstElementChild.childNodes.length === 1) {
+                btnExcel.firstElementChild.appendChild(spanElement);
+            }
+        },
+        createElement: function(tag, props, ...children) {
+            const element = document.createElement(tag);
+            Object.keys(props).forEach( key => element[key] = props[key] );
+            if(children.length > 0){
+                children.forEach( child =>{
+                    element.appendChild(child);
+                });
+            } return element;
+        },
+        afterLoadDataHandler: function() {
+            this.render();
+        },
+        subscribeToDataGridActions: function() {
+        },
+        onDataGridEditorPreparing: function() {
+        },
+        destroy: function() {
         }
-        this.config.columns[2].lookup.dataSource.store = this.elements;
-        this.loadData(this.afterLoadDataHandler);
-    },
-    afterLoadDataHandler: function() {
-        this.render();
-    },
-    subscribeToDataGridActions: function() {
-    },
-    onDataGridEditorPreparing: function() {
-    },
-    destroy: function() {
-    }
-};
+    };
 }());
