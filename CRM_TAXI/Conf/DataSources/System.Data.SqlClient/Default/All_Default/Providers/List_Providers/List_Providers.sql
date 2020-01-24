d5394c506f626e838cc3c383e1b1fae1ecd2f776
MarkusEfre <markus.efr@gmail.com>
2020-01-24 14:31:30 +0200

    TAXI Fix Arrival form and Parts report

--- CRM_TAXI/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Providers/List_Providers/List_Providers.sql
+++ CRM_TAXI/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Providers/List_Providers/List_Providers.sql
@@ -1,11 +1,10 @@
-Select 
-Id,
-[provider] as provider_name,
-provider_conditions 
-
-from Providers
-where 
-#filter_columns#
-#sort_columns#
- 
--- offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+SELECT
+    Id,
+    [provider] AS provider_name,
+    provider_conditions
+FROM
+    dbo.Providers
+WHERE
+    #filter_columns#
+    #sort_columns#
+OFFSET @pageOffsetRows ROWS FETCH NEXT @pageLimitRows ROWS ONLY
