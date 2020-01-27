3b0980ee4968709a130414def395ea4aca214c72
artemkov1994 <artem_kov@email.ua>
2020-01-27 15:40:16 +0200

    добавил applicant_classes_ids

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Emergensy_112/All/GetEmergensyContactByPhone/GetEmergensyContactByPhone.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Emergensy_112/All/GetEmergensyContactByPhone/GetEmergensyContactByPhone.sql
@@ -20,6 +20,13 @@ SELECT TOP (1)
 	  ,ISNULL(st.shortname, N'')+ISNULL(s.name, N'')+ISNULL(N' '+b.name, N'')+
   ISNULL(N', кв.'+LTRIM(p.floor), N'')+ISNULL(N', під`їзд '+LTRIM(p.entrance), N'')+
   ISNULL(N' (код '+p.entercode+N')', N'')+ISNULL(N', поверх: '+LTRIM(p.floor), N'')+ISNULL(N'/'+LTRIM(p.storeysnumber),N'')+ISNULL(N' ,'+p.[exit], N'') event_address
+
+  ,
+  (SELECT STUFF
+  ((SELECT N', '+LTRIM(class_id)
+  FROM [dbo].[PersonClasses] pc0
+  WHERE pc0.[person_id]=p.id
+  FOR XML PATH('')),1,2,N'')) applicant_classes_ids
   
   FROM [CRM_1551_Emergensy_112].[dbo].[Persons] p
   LEFT JOIN [CRM_1551_Analitics].[dbo].[Buildings] b ON p.building_id=b.id
