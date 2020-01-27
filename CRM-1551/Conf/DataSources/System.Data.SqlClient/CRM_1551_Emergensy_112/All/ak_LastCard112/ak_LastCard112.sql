96d82864cdfa4c5f00baea15bb31c7f63f89ddad
artemkov1994 <artem_kov@email.ua>
2020-01-27 14:08:35 +0200

    category_name

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Emergensy_112/All/ak_LastCard112/ak_LastCard112.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Emergensy_112/All/ak_LastCard112/ak_LastCard112.sql
@@ -3,8 +3,10 @@ SELECT e.Id, e.[event_date] receipt_date, e.[longitude], e.[latitude], app.perso
   ISNULL(st.shortname, N'')+ISNULL(s.name, N'')+ISNULL(N' '+b.name, N'')+
   ISNULL(N', кв.'+LTRIM(e.floor), N'')+ISNULL(N', під`їзд '+LTRIM(e.entrance), N'')+
   ISNULL(N' (код '+e.entercode+N')', N'')+ISNULL(N', поверх: '+LTRIM(e.floor), N'')+ISNULL(N'/'+LTRIM(e.storeysnumber),N'') event_address
+  , c.name category_name
 
   FROM [dbo].[Events] e
+  LEFT JOIN [dbo].[Categories] c ON e.category_id=c.id
   LEFT JOIN [dbo].[Persons] app ON e.applicant_id=app.Id
   LEFT JOIN [CRM_1551_Analitics].[dbo].[Buildings] b  ON e.building_id=b.Id
   LEFT JOIN [CRM_1551_Analitics].[dbo].[Streets] s ON b.street_id=s.Id
