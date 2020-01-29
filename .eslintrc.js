48418c6573b3d87e4eed72ecb5a726cc6167aac6
Виталий Носков <v.noskov@domain.local>
2020-01-29 10:20:55 +0200

    Исправил ошибку eslint L is not defined

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "browser": true,
         "es6": true,
-        "jquery": true
+        "jquery": true,
+        "L": true
     },
     "extends": "eslint:recommended",
     "globals": {
