e0a02dd2be98d4ec020b0b3e78d3154e8856bf95
artemkov1994 <artem_kov@email.ua>
2020-02-05 13:25:25 +0200

    1551_638 создал запрос для фильтра, изменил запрос на отображение

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/DepartmentUGL/DepartmentUGL_ExcelSelectRows/DepartmentUGL_ExcelSelectRows.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/DepartmentUGL/DepartmentUGL_ExcelSelectRows/DepartmentUGL_ExcelSelectRows.sql
@@ -1,12 +1,47 @@
-/*declare @dateFrom date;
-declare @dateTo date;
-declare @user_id nvarchar(128);
-declare @is_worked bit;
+/*
+declare @dateFrom DATE='2001-02-01';
+declare @dateTo DATE='2020-12-12';
+declare @user_id nvarchar(128)=N'Вася';
+declare @is_worked BIT;--='true';
+declare @uploaded NVARCHAR(MAX);--=N'N''Вася'',N''Степа''';
 */
-IF @is_worked IS NOT NULL
-BEGIN
+--SELECT convert(nvarchar(10),CONVERT(DATE, @dateFrom))
 
-	SELECT
+DECLARE @dateFrom_f NVARCHAR(100)=
+CASE WHEN N'CONVERT(DATE, Z.[Створено]) >= '''+CONVERT(NVARCHAR(10),CONVERT(DATE, @dateFrom))+N'''' IS NULL THEN N'1=1'
+ELSE N'CONVERT(DATE, Z.[Створено]) >= '''+CONVERT(NVARCHAR(10),CONVERT(DATE, @dateFrom))+N'''' END;
+
+DECLARE @dateTo_f NVARCHAR(100)=
+CASE WHEN N'CONVERT(DATE, Z.[Створено]) <= '''+CONVERT(NVARCHAR(10),CONVERT(DATE, @dateTo))+N'''' IS NULL THEN N'1=1'
+ELSE N'CONVERT(DATE, Z.[Створено]) <= '''+CONVERT(NVARCHAR(10),CONVERT(DATE, @dateTo))+N'''' END;
+
+DECLARE @is_worked_f NVARCHAR(40)=
+CASE WHEN @is_worked IS NULL THEN N'1=1' 
+ELSE N'Z.[Опрацьовано]='+CONVERT(NVARCHAR(10), @is_worked)  END;
+
+DECLARE @uploaded_f NVARCHAR(MAX)=
+CASE WHEN @uploaded IS NULL  THEN N'1=1'
+ELSE N'Z.[Завантажив] IN ('+@uploaded+N')' END;
+
+--SELECT @dateFrom_f, @dateTo_f, @is_worked_f, @uploaded_f,
+--@dateFrom_f+N' AND '+@dateTo_f+N' AND '+@is_worked_f+N' AND '+@uploaded_f
+
+DECLARE @param NVARCHAR(MAX)=@dateFrom_f+N' AND '+@dateTo_f+N' AND '+@is_worked_f+N' AND '+@uploaded_f;
+
+--SELECT @param
+
+IF OBJECT_ID('tempdb..#temp_main') IS NOT NULL
+BEGIN
+	DROP TABLE #temp_main;
+END;
+
+CREATE TABLE #temp_main (Id INT, EnterNumber NVARCHAR(100), RegistrationDate DATETIME, Applicant NVARCHAR(MAX),
+[ADDRESS] NVARCHAR(500), Content NVARCHAR(MAX), QuestionNumber NVARCHAR(20));
+-- через параметр, запрос переписую
+
+DECLARE @exec NVARCHAR(MAX)=
+N'
+SELECT
 		Z.Id
 	   ,Z.[№ звернення] [EnterNumber]
 	   ,Z.[Створено] [RegistrationDate]
@@ -17,27 +52,15 @@ BEGIN
 	   ,q.registration_number QuestionNumber
 	FROM [dbo].[Звернення УГЛ] z
 	LEFT JOIN [dbo].[Questions] q ON Z.Appeals_id=q.appeal_id
-	WHERE CONVERT(DATE, Z.[Створено]) BETWEEN @dateFrom AND @dateTo
-	AND #filter_columns# /*[Опрацював]=@user_id*/
-	AND Z.[Опрацьовано] = @is_worked
+	WHERE '+@param;
 
--- #sort_columns#
---  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+	INSERT INTO #temp_main
+	(Id, EnterNumber, RegistrationDate, Applicant, [ADDRESS], Content, QuestionNumber
+	)
 
-END
+	EXEC(@exec); 
 
-IF @is_worked IS NULL
-BEGIN
-	SELECT
-		Z.Id
-	   ,Z.[№ звернення] [EnterNumber]
-	   ,Z.[Створено] [RegistrationDate]
-	   ,Z.[Заявник] [Applicant]
-	   ,Z.[Адреса] [Address]
-	   ,Z.[Зміст] [Content]
-	   ,Z.[Питання] QuestionNumber
-	FROM [dbo].[Звернення УГЛ] z
-	LEFT JOIN [dbo].[Questions] q ON Z.Appeals_id=q.appeal_id
-	WHERE CONVERT(DATE,Z.[Створено]) BETWEEN @dateFrom AND @dateTo
-	AND #filter_columns# /*[Опрацював]=@user_id*/
-END
+	SELECT Id, EnterNumber, RegistrationDate, Applicant, [ADDRESS], Content, QuestionNumber
+	FROM #temp_main
+    #sort_columns#
+    offset @pageOffsetRows ROWS FETCH NEXT @pageLimitRows ROWS only;
