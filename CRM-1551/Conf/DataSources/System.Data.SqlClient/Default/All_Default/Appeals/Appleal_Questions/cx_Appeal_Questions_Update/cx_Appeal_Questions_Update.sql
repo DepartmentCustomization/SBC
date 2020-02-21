70177fed30881e283ed54283470a64ee8358229c
MarkusEfre <markus.efr@gmail.com>
2020-02-21 12:18:21 +0200

    подправил запросы обновления Question & Assignment

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Appeals/Appleal_Questions/cx_Appeal_Questions_Update/cx_Appeal_Questions_Update.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Appeals/Appleal_Questions/cx_Appeal_Questions_Update/cx_Appeal_Questions_Update.sql
@@ -1,148 +1,149 @@
 -- declare @Question_ControlDate datetime = @control_date
-declare @ass_for_check int = (select last_assignment_for_execution_id from Questions where Id = @Id)
+DECLARE @ass_for_check INT = (
+	SELECT
+		last_assignment_for_execution_id
+	FROM
+		dbo.Questions
+	WHERE
+		Id = @Id
+) ;
+UPDATE
+	[dbo].[Questions]
+SET
+	[control_date] = @control_date,
+	[question_type_id] = @question_type_id,
+	[edit_date] = getutcdate(),
+	[user_edit_id] = @user_edit_id,
+	question_content = @question_content,
+	object_id = @object_id,
+	organization_id = @organization_id,
+	[answer_form_id] = @answer_type_id,
+	[answer_phone] = @answer_phone,
+	[answer_post] = @answer_post,
+	[answer_mail] = @answer_mail
+WHERE
+	Id = @Id ;
+	--  execute define_status_Question  @Id
+	IF @ass_for_check = (
+		SELECT
+			last_assignment_for_execution_id
+		FROM
+			dbo.Questions
+		WHERE
+			Id = @Id
+	) 
+	BEGIN 
+	IF @perfom_id <> (
+		SELECT
+			executor_organization_id
+		FROM
+			dbo.Assignments
+		WHERE
+			Id = (
+				SELECT
+					last_assignment_for_execution_id
+				FROM
+					dbo.Questions
+				WHERE
+					Id = @Id
+			)
+	) 
+BEGIN 
+ DECLARE @output_con TABLE (Id INT);
 
-UPDATE [dbo].[Questions]
-   SET [control_date] = @control_date
-      ,[question_type_id] = @question_type_id
-      ,[edit_date] = getutcdate()
-      ,[user_edit_id] = @user_edit_id
-      ,question_content = @question_content
-      ,object_id = @object_id
-      ,organization_id = @organization_id
-      ,[answer_form_id] = @answer_type_id
-      ,[answer_phone] = @answer_phone
-      ,[answer_post] = @answer_post
-      ,[answer_mail] = @answer_mail
- WHERE Id = @Id
- --  execute define_status_Question  @Id
+DECLARE @assigment INT;
 
+SELECT
+	@assigment = Id
+FROM
+	dbo.Assignments
+WHERE
+	Id = (
+		SELECT
+			last_assignment_for_execution_id
+		FROM
+			dbo.Questions
+		WHERE
+			Id = @Id
+	) ;
+UPDATE
+	[dbo].[Assignments]
+SET
+	[executor_organization_id] = @perfom_id, -- новый исполнитель на кого переопределили
+	--,[execution_date]= @execution_date  
+	executor_person_id = NULL,
+	organization_id = @perfom_id,
+	[edit_date] = getutcdate(),
+	[user_edit_id] = @user_edit_id,
+	LogUpdated_Query = N'cx_Appeal_Questions_Update_ROW33'
+WHERE
+	Id = @assigment ;
 
-if @ass_for_check = (select last_assignment_for_execution_id from Questions where Id = @Id)
-BEGIN
-	if @perfom_id <> (select executor_organization_id from Assignments where Id = (select last_assignment_for_execution_id from Questions where Id = @Id ))
-    	begin
-    		declare @output_con table (Id int);
-			declare @assigment int;
-			select @assigment = Id from Assignments where Id = (select last_assignment_for_execution_id from Questions where Id = @Id )
+UPDATE
+	dbo.AssignmentConsiderations
+SET
+	[edit_date] = getutcdate(),
+	[user_edit_id] = @user_edit_id,
+	[consideration_date] = getutcdate()
+WHERE
+	Id = (
+		SELECT
+			current_assignment_consideration_id
+		FROM
+			dbo.Assignments
+		WHERE
+			Id = (
+				SELECT
+					last_assignment_for_execution_id
+				FROM
+					dbo.Questions
+				WHERE
+					Id = @Id
+			)
+	) ;
+INSERT INTO
+	dbo.AssignmentConsiderations (
+		[assignment_id],
+		[consideration_date],
+		[assignment_result_id],
+		[assignment_resolution_id],
+		[user_id],
+		[edit_date],
+		[user_edit_id],
+		[first_executor_organization_id],
+		create_date,
+		transfer_date
+	) output inserted.Id INTO @output_con([Id])
+VALUES
+(
+		@assigment,
+		GETUTCDATE(),
+		1,
+		NULL,
+		@user_edit_id,
+		GETUTCDATE(),
+		@user_edit_id,
+		@perfom_id -- новый исполнитель на кого переопределили
+,
+		GETUTCDATE(),
+		GETUTCDATE()
+	) ;
+DECLARE @new_con INT ;
 
-    		UPDATE  [dbo].[Assignments]
-    			  set   [executor_organization_id]= @perfom_id -- новый исполнитель на кого переопределили
-    				   --,[execution_date]= @execution_date  
-    				   ,[edit_date]= getutcdate()
-    				   ,[user_edit_id]= @user_edit_id
-    				   ,LogUpdated_Query = N'cx_Appeal_Questions_Update_ROW33'
-    		WHERE Id= @assigment
-    	
-    		update dbo.AssignmentConsiderations
-    				set	
-    				    [edit_date] = getutcdate()
-    				   ,[user_edit_id] = @user_edit_id
-    				   ,[consideration_date] = getutcdate()
-    		where Id = (select current_assignment_consideration_id from Assignments where Id = (select last_assignment_for_execution_id from Questions where Id = @Id ))
-    
-    	
-    		insert into dbo.AssignmentConsiderations
-    			(		[assignment_id]
-    				   ,[consideration_date]
-    				   ,[assignment_result_id]
-    				   ,[assignment_resolution_id]
-    				   ,[user_id]
-    				   ,[edit_date]
-    				   ,[user_edit_id]
-    				   ,[first_executor_organization_id]
-    				   ,create_date
-    				   ,transfer_date
-    			)
-    			output inserted.Id into @output_con([Id])
-    			values( @assigment
-    				    ,GETUTCDATE()
-    				    ,1
-    				    ,null
-    				    ,@user_edit_id
-    				    ,GETUTCDATE()
-    				    ,@user_edit_id
-    				    ,@perfom_id  -- новый исполнитель на кого переопределили
-    				    ,GETUTCDATE()
-    				    ,GETUTCDATE()
-    				   )
-    				    
-            declare  @new_con int
-    		set @new_con = (select top (1) Id from @output_con)
-    		update [Assignments] 
-			set current_assignment_consideration_id = @new_con 
-			,[edit_date] = getutcdate()
-			where Id = @assigment
-    	end
+SET
+	@new_con = (
+		SELECT
+			TOP (1) Id
+		FROM
+			@output_con
+	) ;
+
+UPDATE
+	dbo.[Assignments]
+SET
+	current_assignment_consideration_id = @new_con,
+	[edit_date] = getutcdate()
+WHERE
+	Id = @assigment ;
 END
-
-
-
-/*
-
-UPDATE [dbo].[Questions]
-   SET [control_date] = @control_date
-      ,[question_type_id] = @question_type_id
-      ,[edit_date] = getutcdate()
-      ,[user_edit_id] = @user_edit_id
-      ,question_content = @question_content
-      ,object_id = @object_id
-      ,organization_id = @organization_id
- WHERE Id = @Id
- 
---  execute define_status_Question  @Id
- 
--- update [dbo].[Assignments] 
---     set [executor_organization_id] = @perfom_id
--- where [question_id] = @Id
-
-if @perfom_id <> (select executor_organization_id from Assignments where Id = (select last_assignment_for_execution_id from Questions where Id = @Id ))
-    	begin
-    		declare @output_con table (Id int);
-			declare @assigment int;
-			select @assigment = Id from Assignments where Id = (select last_assignment_for_execution_id from Questions where Id = @Id )
-
-    		UPDATE  [dbo].[Assignments]
-    			  set   [executor_organization_id]= @perfom_id -- новый исполнитель на кого переопределили
-    				   --,[execution_date]= @execution_date  
-    				   ,[edit_date]= getutcdate()
-    				   ,[user_edit_id]= @user_edit_id
-    		WHERE Id= @assigment
-    	
-    		update dbo.AssignmentConsiderations
-    				set	
-    				    [edit_date] = getutcdate()
-    				   ,[user_edit_id] = @user_edit_id
-    				   ,[consideration_date] = getutcdate()
-    		where Id = (select current_assignment_consideration_id from Assignments where Id = (select last_assignment_for_execution_id from Questions where Id = @Id ))
-    
-    	
-    		insert into dbo.AssignmentConsiderations
-    			(		[assignment_id]
-    				   ,[consideration_date]
-    				   ,[assignment_result_id]
-    				   ,[assignment_resolution_id]
-    				   ,[user_id]
-    				   ,[edit_date]
-    				   ,[user_edit_id]
-    				   ,[first_executor_organization_id]
-    				   ,create_date
-    				   ,transfer_date
-    			)
-    			output inserted.Id into @output_con([Id])
-    			values( @assigment
-    				    ,GETUTCDATE()
-    				    ,1
-    				    ,null
-    				    ,@user_edit_id
-    				    ,GETUTCDATE()
-    				    ,@user_edit_id
-    				    ,@perfom_id  -- новый исполнитель на кого переопределили
-    				    ,GETUTCDATE()
-    				    ,GETUTCDATE()
-    				   )
-    				    
-            declare  @new_con int
-    		set @new_con = (select top (1) Id from @output_con)
-    		update [Assignments] set current_assignment_consideration_id = @new_con where Id = @assigment
-    	end
-*/
+END
