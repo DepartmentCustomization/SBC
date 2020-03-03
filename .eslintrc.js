45d63f723a4c225211dcbd8f1738d25e21a39632
Yehor <egorkatornado@gmail.com>
2020-03-03 16:46:55 +0200

    update codeReview

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
         "SharedArrayBuffer": "readonly",
         "L": true
     },
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module",
