c4dc1f6c2a1021922000adae2fe133ea80fb65a0
Yehor <egorkatornado@gmail.com>
2020-03-04 12:35:25 +0200

    update start_avr_page

--- CRM-AVR/Conf/Pages/Dashboards/Groups/TestUsers/start_avr_page/Widgets/tabs_CustomWidget.js
+++ CRM-AVR/Conf/Pages/Dashboards/Groups/TestUsers/start_avr_page/Widgets/tabs_CustomWidget.js
@@ -5,23 +5,7 @@
         formatTitle: function() {},
         customConfig:
                 `
-                <style>
-                    #container{
-                        display: flex;
-                        justify-content: space-around;                    
-                    }
-                    .btn{
-                        text-align: center;
-                        font-size: 17px;
-                        font-weight: 600;
-                        cursor: pointer;
-                        line-height: 14px;
-                        padding: 0 10px;
-                        height: 32px;                        
-                    }
-                </style>
-                
-                <div id='container'></div>
+                <div id='container1'></div>
                 `
         ,
         init: function() {
@@ -49,9 +33,25 @@
             const yyyy = date.getFullYear();
             const mm = date.getMonth() + 1;
             const month = yyyy + '-' + mm + '-01%2000:00:00';
-            let container = document.getElementById('container');
-            const btn_schedule = this.createElement('button', { id: 'btn_schedule', className: 'btn', url: 'TablingPerson', location: 'dashboard', innerText: 'Табель поточного місяця'});
-            const btn_createAppeal = this.createElement('button', { id: 'btn_maps', className: 'btn', url: 'Claims/add', location: 'sections', innerText: 'Створити Заявку'});
+            let container = document.getElementById('container1');
+            const btn_schedule = this.createElement('button',
+                {
+                    id: 'btn_schedule',
+                    className: 'btn',
+                    url: 'TablingPerson',
+                    location: 'dashboard',
+                    innerText: 'Табель поточного місяця'
+                }
+            );
+            const btn_createAppeal = this.createElement('button',
+                {
+                    id: 'btn_maps',
+                    className: 'btn',
+                    url: 'Claims/add',
+                    location: 'sections',
+                    innerText: 'Створити Заявку'
+                }
+            );
             const btn_clearFilter = this.createElement('button', { id: 'btn_schedule', className: 'btn', innerText: 'Зняти маркер'});
             const tab_schedule = this.createElement('div', { id: 'tab_table'}, btn_schedule);
             const tab_appeal = this.createElement('div', { id: 'tab_maps'}, btn_createAppeal);
@@ -62,13 +62,21 @@
             btn_schedule.addEventListener('click', event => {
                 let target = event.currentTarget;
                 const params = 'orgId=' + this.org_id + '&month=' + month;
-                window.open(location.origin + localStorage.getItem('VirtualPath') + '/' + target.location + '/page/' + target.url + '?' + params);
+                window.open(
+                    location.origin +
+                    localStorage.getItem('VirtualPath') + '/' +
+                    target.location + '/page/' + target.url + '?' + params
+                );
             });
             btn_createAppeal.addEventListener('click', event => {
                 let target = event.currentTarget;
-                window.open(location.origin + localStorage.getItem('VirtualPath') + '/' + target.location + '/' + target.url);
+                window.open(
+                    location.origin +
+                    localStorage.getItem('VirtualPath') + '/' +
+                    target.location + '/' + target.url
+                );
             });
-            btn_clearFilter.addEventListener('click', event => {
+            btn_clearFilter.addEventListener('click', () => {
                 this.messageService.publish({ name: 'clearSelectedRows'});
             });
         },
