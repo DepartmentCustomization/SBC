99586c9a0ae394c68ea6d30859b1287a3108a7cb
artemkov1994 <artem_kov@email.ua>
2020-03-05 10:34:09 +0200

    фильтрация

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/db_Urbio/urbio_db_filter_processed/urbio_db_filter_processed.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/db_Urbio/urbio_db_filter_processed/urbio_db_filter_processed.sql
@@ -3,8 +3,9 @@ FROM
 (SELECT 1 Id, 'false' [Row], N'Неопрацьовані' [Name]
   UNION ALL
   SELECT 2 Id, 'true' [Row], N'Опрацьовані' [Name]
-  UNION ALL
-  SELECT 3 Id, null [Row], N'Усі' [Name]) t
+  -- UNION ALL
+  -- SELECT 3 Id, null [Row], N'Усі' [Name]
+  ) t
   WHERE #filter_columns#
   #sort_columns#
   OFFSET @pageOffsetRows ROWS FETCH NEXT @pageLimitRows ROWS ONLY
