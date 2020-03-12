ff46984f619a17831a673e5bd1b954da807aeace
Yehor <egorkatornado@gmail.com>
2020-03-12 14:49:02 +0200

    689 is done

--- CRM-1551/Conf/Pages/Dashboards/Groups/StartPage/performers/Widgets/Plan_Programs_NativeDataGridWidget.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/StartPage/performers/Widgets/Plan_Programs_NativeDataGridWidget.js
@@ -94,12 +94,18 @@
             document.getElementById('table10_Plan_Programs').style.display = 'none';
             this.sub = this.messageService.subscribe('clickOnTable2', this.changeOnTable, this);
             this.config.masterDetail.template = this.createMasterDetail.bind(this);
+            this.config.onCellPrepared = this.onCellPrepared.bind(this);
             this.dataGridInstance.onCellClick.subscribe(e => {
-                if(e.column) {
-                    if(e.column.dataField === 'registration_number' && e.row !== undefined) {
-                        window.open(String(location.origin + localStorage.getItem('VirtualPath') + '/sections/Assignments/edit/' + e.key));
-                    }
-                }
+                this.messageService.publish({
+                    name: 'openForm',
+                    row: e
+                });
+            });
+        },
+        onCellPrepared: function(options) {
+            this.messageService.publish({
+                name: 'onCellPrepared',
+                options: options
             });
         },
         changeOnTable: function(message) {
