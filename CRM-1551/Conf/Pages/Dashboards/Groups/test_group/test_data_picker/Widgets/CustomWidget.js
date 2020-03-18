5838a1b130990e3754c0d861d2198dbac4e6af47
Егор Синьков <egorkatornado@gmail.com>
2020-03-18 12:59:05 +0200

    update test_data_picker

--- CRM-1551/Conf/Pages/Dashboards/Groups/test_group/test_data_picker/Widgets/CustomWidget.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/test_group/test_data_picker/Widgets/CustomWidget.js
@@ -16,13 +16,34 @@
                 }
             };
             this.messageService.publish(msg);
-            this.sub = this.messageService.subscribe('GlobalFilterChanged', this.getFiltersParam, this);
+            this.subscribers.push(this.messageService.subscribe('GlobalFilterChanged', this.getFiltersParam, this));
+        },
+        createElement: function(tag, props, ...children) {
+            const element = document.createElement(tag);
+            Object.keys(props).forEach(key => element[key] = props[key]);
+            const zeroLength = 0;
+            if(children.length > zeroLength) {
+                children.forEach(child =>{
+                    element.appendChild(child);
+                });
+            }
+            return element;
+        },
+        afterViewInit: function() {
+            const modalContainer = document.getElementById('modalContainer');
+            const btn = this.createElement('button', {
+                innerText: 'CLick on me to go to section!'
+            });
+            modalContainer.appendChild(btn);
+            btn.addEventListener('click', () => {
+                this.goToDashboard('StartPage_operator');
+            })
         },
         getFiltersParam: function(message) {
             this.d1 = message.package.value.values.find(f => f.name === 'd1').value;
-            this.d2 = message.package.value.values.find(f => f.name === 'd2').value;
-            this.d3 = message.package.value.values.find(f => f.name === 'd3').value;
-            this.d4 = message.package.value.values.find(f => f.name === 'd4').value;
+            this.d2 = message.package.value.values.find(f => f.name === 'd2_calendar').value;
+            this.d3 = message.package.value.values.find(f => f.name === 'd3_time').value;
+            this.d4 = message.package.value.values.find(f => f.name === 'd4_date').value;
             this.appeals_district = message.package.value.values.find(f => f.name === 'appeals_district').value;
             this.appeals_enter_number = message.package.value.values.find(f => f.name === 'appeals_enter_number').value;
             this.overdue = message.package.value.values.find(f => f.name === 'overdue').value;
