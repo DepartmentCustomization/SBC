a36dc81371a60e2617b0311f6143dd8adbb15cd8
MarkusEfre <markus.efr@gmail.com>
2020-03-26 15:31:34 +0200

    исправление по спец службам

--- CRM-AVR/Conf/Forms/Form_OutsideMen/Form_OutsideMen.form.js
+++ CRM-AVR/Conf/Forms/Form_OutsideMen/Form_OutsideMen.form.js
@@ -1,25 +1,24 @@
 (function() {
     return {
+        closeNum: 10,
         init: function() {
             this.form.disableControl('contact_number');
             this.form.onControlValueChanged('company_id', this.sampleFiz);
         },
         sampleFiz: function(org_id) {
-        // let org =  [{ parameterCode: '@org_id', parameterValue: org_id }];
-        // this.form.setControlParameterValues('fiz_id', org);
+            let orgParam = [{ parameterCode: '@OrgID', parameterValue: org_id }];
+            this.form.setControlParameterValues('fiz_name', orgParam);
             const org_id_n = {
                 queryCode: 'phone_number_outsideMan',
                 parameterValues:[{key: '@org_id',value: org_id}]
             };
             this.queryExecutor.getValues(org_id_n).subscribe(data =>{
-            // debugger;
-            // this.form.setControlValue('contact_number', null);
                 this.form.setControlValue('contact_number', data.rows[0].values[0]);
             });
         },
         validate: function() {
             const order_status = this.form.getControlValue('order_stat_id')
-            if (order_status == 10) {
+            if (order_status === this.closeNum) {
                 return 'У виконаному виїзді вносити правки заборонено'
             }
             return true
