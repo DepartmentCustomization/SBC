02f225fd98cac190953809d11188a4041acad148
Егор Синьков <egorkatornado@gmail.com>
2020-04-03 12:36:34 +0300

    Task 708 is done

--- CRM-1551/Conf/Pages/Dashboards/Groups/StartPage/performer_new_organizations/Widgets/NativeDataGridWidget-6.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/StartPage/performer_new_organizations/Widgets/NativeDataGridWidget-6.js
@@ -30,10 +30,6 @@
                     format: 'dd.MM.yyyy HH:mm'
                 }
             ],
-            export: {
-                enabled: true,
-                fileName: 'Excel'
-            },
             searchPanel: {
                 visible: false,
                 highlightCaseSensitive: true
@@ -94,6 +90,7 @@
             document.getElementById('table10_Plan_Programs').style.display = 'none';
             this.sub = this.messageService.subscribe('clickOnTable2', this.changeOnTable, this);
             this.config.masterDetail.template = this.createMasterDetail.bind(this);
+            this.config.onToolbarPreparing = this.createTableButton.bind(this);
             this.dataGridInstance.onCellClick.subscribe(e => {
                 if(e.column) {
                     if(e.column.dataField === 'registration_number' && e.row !== undefined) {
@@ -224,6 +221,243 @@
         },
         destroy: function() {
             this.sub.unsubscribe();
+        },
+        createTableButton: function(e) {
+            let toolbarItems = e.toolbarOptions.items;
+            toolbarItems.push({
+                widget: 'dxButton',
+                options: {
+                    icon: 'exportxlsx',
+                    type: 'default',
+                    text: 'Excel',
+                    onClick: function(e) {
+                        e.event.stopImmediatePropagation();
+                        this.exportToExcel();
+                    }.bind(this)
+                },
+                location: 'after'
+            });
+        },
+        exportToExcel: function() {
+            let exportQuery = {
+                queryCode: this.config.query.code,
+                limit: -1,
+                parameterValues: this.config.query.parameterValues
+            };
+            this.queryExecutor(exportQuery, this.myCreateExcel, this);
+        },
+        myCreateExcel: function(data) {
+            this.showPagePreloader('Зачекайте, формується документ');
+            this.indexArr = [];
+            let column_registration_number = { name: 'registration_number', index: 0 };
+            let column_zayavnyk = { name: 'zayavnyk', index: 1 };
+            let column_ZayavnykZmist = { name: 'zayavnyk_zmist', index: 2 };
+            let column_controlDate = { name: 'controlDate', index: 3 };
+            let column_adress = { name: 'adress', index: 4 };
+            this.indexArr = [
+                column_registration_number,
+                column_zayavnyk,
+                column_ZayavnykZmist,
+                column_controlDate,
+                column_adress
+            ];
+            const workbook = this.createExcel();
+            const worksheet = workbook.addWorksheet('Заявки', {
+                pageSetup:{
+                    orientation: 'landscape',
+                    fitToPage: false
+                }
+            });
+            worksheet.pageSetup.margins = {
+                left: 0.4, right: 0.3,
+                top: 0.4, bottom: 0.4,
+                header: 0.0, footer: 0.0
+            };
+            let cellInfoCaption = worksheet.getCell('A1');
+            cellInfoCaption.value = 'Інформація';
+            let cellInfo = worksheet.getCell('A2');
+            cellInfo.value =
+                'про звернення громадян, що надійшли до Контактного центру  міста Києва. Термін виконання …';
+            let cellPeriod = worksheet.getCell('A3');
+            cellPeriod.value = 'Період вводу з (включно) : дата з … дата по … .';
+            let cellNumber = worksheet.getCell('A4');
+            cellNumber.value = 'Реєстраційний № РДА …';
+            worksheet.mergeCells('A1:F1');
+            worksheet.mergeCells('A2:F2');
+            worksheet.mergeCells('A3:F3');
+            worksheet.getRow(1).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 10,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(1).alignment = { vertical: 'middle', horizontal: 'center' };
+            worksheet.getRow(2).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 10,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(2).alignment = { vertical: 'middle', horizontal: 'center' };
+            worksheet.getRow(3).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 10,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(3).alignment = { vertical: 'middle', horizontal: 'left' };
+            worksheet.getRow(4).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 10,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(5).alignment = { vertical: 'middle', horizontal: 'left' };
+            let indexArr = this.indexArr;
+            let rows = [];
+            let captions = [];
+            let columnsHeader = [];
+            let columnNumber = { key: 'number', width: 5 }
+            columnsHeader.push(columnNumber);
+            let rowNumber = '№ з/п';
+            captions.push(rowNumber);
+            indexArr.forEach(el => {
+                let obj = {}
+                if (el.name === 'registration_number') {
+                    obj.key = 'registration_number';
+                    obj.width = 10;
+                    obj.height = 20;
+                    captions.push('Номер, дата, час');
+                } else if(el.name === 'zayavnyk_zmist') {
+                    obj.key = 'zayavnyk_zmist';
+                    obj.width = 44;
+                    captions.push('Суть питання');
+                } else if(el.name === 'zayavnyk') {
+                    obj.key = 'zayavnyk';
+                    obj.width = 30;
+                    captions.push('Заявник');
+                } else if(el.name === 'controlDate') {
+                    obj.key = 'controlDate';
+                    obj.width = 16;
+                    captions.push('Дата контролю');
+                } else if(el.name === 'adress') {
+                    obj.key = 'adress';
+                    obj.width = 21;
+                    captions.push('Місце проблеми (Об\'єкт)');
+                }
+                columnsHeader.push(obj);
+            });
+            worksheet.getRow(5).values = captions;
+            worksheet.columns = columnsHeader;
+            this.addetedIndexes = [];
+            let indexRegistrationNumber = data.columns.findIndex(el => el.code.toLowerCase() === 'registration_number');
+            let indexZayavnikName = data.columns.findIndex(el => el.code.toLowerCase() === 'zayavnyk');
+            let indexAdress = data.columns.findIndex(el => el.code.toLowerCase() === 'adress');
+            let indexControlDate = data.columns.findIndex(el => el.code.toLowerCase() === 'control_date');
+            let indexQuestionContent = data.columns.findIndex(el => el.code.toLowerCase() === 'zayavnyk_zmist');
+            let indexAdressZ = data.columns.findIndex(el => el.code.toLowerCase() === 'zayavnyk_adress');
+            for(let j = 0; j < data.rows.length; j++) {
+                const row = data.rows[j];
+                let rowItem = { number: j + 1 };
+                for(let i = 0; i < indexArr.length; i++) {
+                    let el = indexArr[i];
+                    let cdValue = this.changeDateTimeValues(row.values[indexControlDate]);
+                    if (el.name === 'registration_number') {
+                        rowItem.registration_number = row.values[indexRegistrationNumber];
+                    } else if(el.name === 'zayavnyk') {
+                        rowItem.zayavnyk = row.values[indexZayavnikName] + ', ' + row.values[indexAdressZ];
+                    } else if(el.name === 'zayavnyk_zmist') {
+                        rowItem.zayavnyk_zmist = 'Зміст: ' + row.values[indexQuestionContent];
+                    } else if(el.name === 'controlDate') {
+                        rowItem.controlDate = cdValue;
+                    } else if(el.name === 'adress') {
+                        rowItem.adress = row.values[indexAdress];
+                    }
+                }
+                rows.push(rowItem);
+            }
+            rows.forEach(el => {
+                let number = el.number + '.'
+                let row = {
+                    number: number,
+                    registration_number: el.registration_number,
+                    zayavnyk: el.zayavnyk,
+                    zayavnyk_zmist: el.zayavnyk_zmist,
+                    controlDate: el.controlDate,
+                    adress: el.adress
+                }
+                worksheet.addRow(row);
+            });
+            for(let i = 0; i < rows.length + 1; i++) {
+                let number = i + 5;
+                const row = worksheet.getRow(number);
+                row.height = 100;
+                worksheet.getRow(number).border = {
+                    top: {style:'thin'},
+                    left: {style:'thin'},
+                    bottom: {style:'thin'},
+                    right: {style:'thin'}
+                };
+                worksheet.getRow(number).alignment = {
+                    vertical: 'middle',
+                    horizontal: 'center',
+                    wrapText: true
+                };
+                worksheet.getRow(number).font = {
+                    name: 'Times New Roman',
+                    family: 4, size: 10,
+                    underline: false,
+                    bold: false ,
+                    italic: false
+                };
+            }
+            worksheet.getRow(2).border = {
+                bottom: {style:'thin'}
+            };
+            worksheet.getRow(5).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 10,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(5).alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+            this.helperFunctions.excel.save(workbook, 'Заявки', this.hidePagePreloader);
+        },
+        changeDateTimeValues: function(value) {
+            let trueDate = ' ';
+            if(value !== null) {
+                let date = new Date(value);
+                let dd = date.getDate();
+                let MM = date.getMonth();
+                let yyyy = date.getFullYear();
+                let HH = date.getUTCHours()
+                let mm = date.getMinutes();
+                MM += 1;
+                if((dd.toString()).length === 1) {
+                    dd = '0' + dd;
+                }
+                if((MM.toString()).length === 1) {
+                    MM = '0' + MM;
+                }
+                if((HH.toString()).length === 1) {
+                    HH = '0' + HH;
+                }
+                if((mm.toString()).length === 1) {
+                    mm = '0' + mm;
+                }
+                trueDate = dd + '.' + MM + '.' + yyyy;
+            }
+            return trueDate;
         }
     };
 }());
