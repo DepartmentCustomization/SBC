2e1b1f5e38dd6adc71a2d7d1988f37ef410ff010
Егор Синьков <egorkatornado@gmail.com>
2020-04-21 11:18:36 +0300

    add new dashboard  and change queries

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/EtalonReferenceDay/db_ReferenceEtalonDays/db_ReferenceEtalonDays.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/EtalonReferenceDay/db_ReferenceEtalonDays/db_ReferenceEtalonDays.sql
@@ -1,8 +1,8 @@
---   DECLARE @date_from DATE='2018-01-01';
---   DECLARE @date_to DATE='2020-12-12';
+--   DECLARE @dateFrom DATE='2018-01-01';
+--   DECLARE @dateTo DATE='2020-12-12';
 
   DECLARE @date DATE = GETUTCDATE();--CONVERT(DATE,'2019-09-25 17:37:06.090');
-
+--DECLARE @date DATE = CONVERT(DATE,'2019-09-25 17:37:06.090');
   SELECT [QuestionTypes].Id,
   [QuestionTypes].Id QuestionTypes_Id, 
   [QuestionTypes].name QuestionTypes_Name, --1
@@ -21,18 +21,17 @@
   (SELECT QuestionTypeId, AVG(CONVERT(FLOAT,EtalonDaysToExecution)) avg_EtalonDaysToExecution
   FROM [dbo].[Rating_EtalonDaysToExecution]
   WHERE EtalonDaysToExecution IS NOT NULL AND CONVERT(DATE, [Rating_EtalonDaysToExecution].[CreatedAt]) 
-  BETWEEN CONVERT(DATE,@date_from) AND CONVERT(DATE,@date_to)
+  BETWEEN CONVERT(DATE,@dateFrom) AND CONVERT(DATE,@dateTo)
   GROUP BY QuestionTypeId) avg_EtalonDaysToExecution ON [Rating_EtalonDaysToExecution].QuestionTypeId=avg_EtalonDaysToExecution.QuestionTypeId
 
   LEFT JOIN 
   (SELECT QuestionTypeId, AVG(CONVERT(FLOAT,EtalonDaysToExplain)) avg_EtalonDaysToExplain
   FROM [dbo].[Rating_EtalonDaysToExecution]
   WHERE EtalonDaysToExplain IS NOT NULL AND CONVERT(DATE, [Rating_EtalonDaysToExecution].[CreatedAt]) 
-  BETWEEN CONVERT(DATE,@date_from) AND CONVERT(DATE,@date_to)
+  BETWEEN CONVERT(DATE,@dateFrom) AND CONVERT(DATE,@dateTo)
   GROUP BY QuestionTypeId) avg_EtalonDaysToExplain ON [Rating_EtalonDaysToExecution].QuestionTypeId=avg_EtalonDaysToExplain.QuestionTypeId
 
   WHERE CONVERT(DATE, [Rating_EtalonDaysToExecution].[CreatedAt])=@date
 
-  AND #filter_columns#
-  #sort_columns#
- offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+--   AND #filter_columns#
+--   #sort_columns#
