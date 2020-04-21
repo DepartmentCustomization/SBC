3c5c72ec04bfbef01a8c63b73113d13682ea5ef2
MarkusEfre <markus.efr@gmail.com>
2020-04-21 13:22:43 +0300

    настроил некоторые детали формы заявителя

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Applicants/Applicants_detail/det_ConsultationApplication/det_ConsultationApplication.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Applicants/Applicants_detail/det_ConsultationApplication/det_ConsultationApplication.sql
@@ -1,22 +1,19 @@
-  select [Consultations].id ConsultationsId
-		 ,[Consultations].phone_number
-		 ,[ConsultationTypes].name ConsultationType
-		 ,[Events].Id EventId
-		 ,[Events].name EventsName
-		 ,[Consultations].[registration_date]
-  --from [CRM_1551_Analitics].[dbo].[Applicants]
-  --left join [CRM_1551_Analitics].[dbo].[Appeals] on [Applicants].Id=[Appeals].applicant_id
-  --left join [CRM_1551_Analitics].[dbo].[Consultations] on [Appeals].Id=[Consultations].appeal_id
+-- DECLARE @ApplicantsId INT = 27;
 
-  from [dbo].[Consultations]
-  left join [dbo].[Appeals] on [Appeals].id =  Consultations.appeal_id
-  left join Applicants on Applicants.Id = Appeals.applicant_id
-  left join [dbo].[ConsultationTypes] on [Consultations].consultation_type_id=[ConsultationTypes].Id
-  left join [dbo].[Events] on [Consultations].event_id=[Events].Id
-
-
-  where [Applicants].id =@ApplicantsId
-  and 
-    #filter_columns#
-     #sort_columns#
-offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+SELECT
+  [Consultations].id ConsultationsId,
+  [Consultations].phone_number,
+  [ConsultationTypes].name ConsultationType,
+  [Events].Id EventId,
+  [Consultations].[registration_date]
+FROM
+  [dbo].[Consultations]
+  LEFT JOIN [dbo].[Appeals] ON [Appeals].id = Consultations.appeal_id
+  LEFT JOIN Applicants ON Applicants.Id = Appeals.applicant_id
+  LEFT JOIN [dbo].[ConsultationTypes] ON [Consultations].consultation_type_id = [ConsultationTypes].Id
+  LEFT JOIN [dbo].[Events] ON [Consultations].event_id = [Events].Id
+WHERE
+  [Applicants].id = @ApplicantsId
+  AND #filter_columns#
+      #sort_columns#
+ OFFSET @pageOffsetRows ROWS FETCH NEXT @pageLimitRows ROWS ONLY ;
--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Applicants/Applicants_detail/det_PhoneApplication/det_PhoneApplication.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Applicants/Applicants_detail/det_PhoneApplication/det_PhoneApplication.sql
@@ -1,34 +1,41 @@
---declare @phone nvarchar(100);
+-- DECLARE @phone NVARCHAR(400) = '0634587456, 0442545698, 0672674525';
 
-select [Applicants].Id
-	  ,[Applicants].full_name
--- 	  ,[Districts].name DistrictsName
--- 	  ,[Streets].name StrictName
--- 	  ,[Buildings].number BuildNumber
--- 	  ,[LiveAddress].house_block
--- 	  ,[LiveAddress].entrance
--- 	  ,[LiveAddress].flat
+SET @phone = REPLACE(@phone, ', ', ',');
 
-	  ,concat('р-н. ' + [Districts].name, ', '+ StreetTypes.shortname +' ' + [Streets].name, 
-	  ', буд.' + [Buildings].name, ', під.' + rtrim([LiveAddress].entrance), ', кв.' + [LiveAddress].flat
-	   ) as Building
+DECLARE @phonesTab TABLE (Num NVARCHAR(10));
+INSERT INTO @phonesTab(Num)
+SELECT 
+	[value] 
+FROM string_split(@phone, ',');
 
-	  ,[ApplicantPrivilege].Name Privilege
-	  ,[SocialStates].name [SocialStates]
-
-  from [CRM_1551_Analitics].[dbo].[Applicants]
- left join [dbo].[ApplicantPhones] on [Applicants].Id=[ApplicantPhones].applicant_id
-  left join [dbo].[PhoneTypes] on [ApplicantPhones].phone_type_id=[PhoneTypes].Id
-  left join [dbo].[LiveAddress] on [Applicants].Id=[LiveAddress].[applicant_id]
-  left join [dbo].[Buildings] on [LiveAddress].building_id=[Buildings].Id
-  left join [dbo].[Districts] on [Buildings].district_id=Districts.Id
-  left join [dbo].[Streets] on [Buildings].street_id=[Streets].Id
-  left join StreetTypes on StreetTypes.Id = Streets.street_type_id
-  left join [dbo].[ApplicantTypes] on [Applicants].applicant_type_id=[ApplicantTypes].Id
-
-
-  left join [dbo].[CategoryType] on [Applicants].category_type_id=[CategoryType].Id
-  left join [dbo].[ApplicantPrivilege] on [Applicants].applicant_privilage_id=[ApplicantPrivilege].Id
-  left join [dbo].[SocialStates] on [Applicants].social_state_id=[SocialStates].Id
-
-  where ApplicantPhones.phone_number=@phone
+SELECT
+DISTINCT
+    [Applicants].Id,
+    [Applicants].full_name,
+    concat(
+        'р-н. ' + [Districts].name,
+        ', ' + StreetTypes.shortname + ' ' + [Streets].name,
+        ', буд.' + [Buildings].name,
+        ', під.' + rtrim([LiveAddress].entrance),
+        ', кв.' + [LiveAddress].flat
+    ) AS Building,
+    [ApplicantPrivilege].Name Privilege,
+    [SocialStates].name [SocialStates]
+FROM
+    [CRM_1551_Analitics].[dbo].[Applicants]
+    LEFT JOIN [dbo].[ApplicantPhones] ON [Applicants].Id = [ApplicantPhones].applicant_id
+    LEFT JOIN [dbo].[PhoneTypes] ON [ApplicantPhones].phone_type_id = [PhoneTypes].Id
+    LEFT JOIN [dbo].[LiveAddress] ON [Applicants].Id = [LiveAddress].[applicant_id]
+	AND LiveAddress.main = 1
+    LEFT JOIN [dbo].[Buildings] ON [LiveAddress].building_id = [Buildings].Id
+    LEFT JOIN [dbo].[Districts] ON [Buildings].district_id = Districts.Id
+    LEFT JOIN [dbo].[Streets] ON [Buildings].street_id = [Streets].Id
+    LEFT JOIN StreetTypes ON StreetTypes.Id = Streets.street_type_id
+    LEFT JOIN [dbo].[ApplicantTypes] ON [Applicants].applicant_type_id = [ApplicantTypes].Id
+    LEFT JOIN [dbo].[CategoryType] ON [Applicants].category_type_id = [CategoryType].Id
+    LEFT JOIN [dbo].[ApplicantPrivilege] ON [Applicants].applicant_privilage_id = [ApplicantPrivilege].Id
+    LEFT JOIN [dbo].[SocialStates] ON [Applicants].social_state_id = [SocialStates].Id
+WHERE
+    ApplicantPhones.phone_number IN (SELECT 
+										 [Num]
+									 FROM @phonesTab);
