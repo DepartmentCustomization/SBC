bf47bdfadf0178d379499b572c34f8e9752e672f
artemkov1994 <artem_kov@email.ua>
2020-05-14 13:14:17 +0300

    1551_761 изменил сегодняшний день согласно комментарию

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/EtalonReferenceDay/db_ReferenceEtalonDays/db_ReferenceEtalonDays.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/EtalonReferenceDay/db_ReferenceEtalonDays/db_ReferenceEtalonDays.sql
@@ -1,8 +1,16 @@
---   DECLARE @dateFrom DATE='2018-01-01';
---   DECLARE @dateTo DATE='2020-12-12';
+   --DECLARE @dateFrom DATE='2018-01-01';
+   --DECLARE @dateTo DATE='2020-12-12';
 
-  DECLARE @date DATE = GETUTCDATE();--CONVERT(DATE,'2019-09-25 17:37:06.090');
+  DECLARE @date DATE = --GETUTCDATE();--CONVERT(DATE,'2019-09-25 17:37:06.090');
 --DECLARE @date DATE = CONVERT(DATE,'2019-09-25 17:37:06.090');
+  (SELECT TOP 1 [DateStart]
+  FROM (
+  select DISTINCT [DateStart], DATEDIFF(DAY,[DateStart],CONVERT(DATE,GETUTCDATE())) diff, ABS(DATEDIFF(DAY,[DateStart],CONVERT(DATE,GETUTCDATE()))) abs_diff
+  from [dbo].[Rating_EtalonDaysToExecution]
+  --order by ABS(DATEDIFF(DAY,[DateStart],CONVERT(DATE,GETUTCDATE()))), DATEDIFF(DAY,[DateStart],CONVERT(DATE,GETUTCDATE())) DESC
+  ) t
+  order by abs_diff, diff DESC)
+
   SELECT [QuestionTypes].Id,
   [QuestionTypes].Id QuestionTypes_Id, 
   [QuestionTypes].name QuestionTypes_Name, --1
@@ -20,18 +28,19 @@
   LEFT JOIN 
   (SELECT QuestionTypeId, AVG(CONVERT(FLOAT,EtalonDaysToExecution)) avg_EtalonDaysToExecution
   FROM [dbo].[Rating_EtalonDaysToExecution]
-  WHERE EtalonDaysToExecution IS NOT NULL AND CONVERT(DATE, [Rating_EtalonDaysToExecution].[CreatedAt]) 
+  WHERE EtalonDaysToExecution IS NOT NULL AND [Rating_EtalonDaysToExecution].[DateStart] 
   BETWEEN CONVERT(DATE,@dateFrom) AND CONVERT(DATE,@dateTo)
   GROUP BY QuestionTypeId) avg_EtalonDaysToExecution ON [Rating_EtalonDaysToExecution].QuestionTypeId=avg_EtalonDaysToExecution.QuestionTypeId
 
   LEFT JOIN 
   (SELECT QuestionTypeId, AVG(CONVERT(FLOAT,EtalonDaysToExplain)) avg_EtalonDaysToExplain
   FROM [dbo].[Rating_EtalonDaysToExecution]
-  WHERE EtalonDaysToExplain IS NOT NULL AND CONVERT(DATE, [Rating_EtalonDaysToExecution].[CreatedAt]) 
+  WHERE EtalonDaysToExplain IS NOT NULL AND [Rating_EtalonDaysToExecution].[DateStart] 
   BETWEEN CONVERT(DATE,@dateFrom) AND CONVERT(DATE,@dateTo)
   GROUP BY QuestionTypeId) avg_EtalonDaysToExplain ON [Rating_EtalonDaysToExecution].QuestionTypeId=avg_EtalonDaysToExplain.QuestionTypeId
 
-  WHERE CONVERT(DATE, [Rating_EtalonDaysToExecution].[CreatedAt])=@date
+  WHERE CONVERT(DATE, [Rating_EtalonDaysToExecution].[DateStart])=@date
 
---   AND #filter_columns#
---   #sort_columns#
+   AND #filter_columns#
+   order by 1 --#sort_columns#
+   offset @pageOffsetRows rows fetch next @pageLimitRows rows only
