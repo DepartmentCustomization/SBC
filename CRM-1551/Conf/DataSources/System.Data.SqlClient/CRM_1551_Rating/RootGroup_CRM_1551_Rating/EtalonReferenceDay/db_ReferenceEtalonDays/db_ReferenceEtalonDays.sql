6176712e8fe55bbf8224b84f0e315dd99ad6c1d9
artemkov1994 <artem_kov@email.ua>
2020-05-25 13:33:50 +0300

    1551_761 комментарий, но с РДА не ясно

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/EtalonReferenceDay/db_ReferenceEtalonDays/db_ReferenceEtalonDays.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/EtalonReferenceDay/db_ReferenceEtalonDays/db_ReferenceEtalonDays.sql
@@ -11,26 +11,64 @@
   ) t
   order by abs_diff, diff DESC)
 
+
+  /*Стан - На перевірці, результат - Виконано. K 3*/
+
+  IF OBJECT_ID('tempdb..#temp_column3') IS NOT NULL	DROP TABLE #temp_column3;
+
+
+  select [Questions].question_type_id, convert(numeric(8,2), avg(convert(float,datediff(dd, [Assignments].registration_date, l.Log_Date)))) count_day
+  into #temp_column3
+  from [CRM_1551_Analitics].[dbo].[Assignments]
+  INNER JOIN
+  (select [assignment_id], MIN([Log_Date]) [Log_Date]
+  from [CRM_1551_Analitics].[dbo].[Assignment_History]
+  where [assignment_state_id]=3 /*На перевірці*/ and [AssignmentResultsId]=4 /*Виконано*/
+  group by [assignment_id]) l on [Assignments].Id=l.[assignment_id]
+  INNER JOIN [CRM_1551_Analitics].[dbo].[Questions] ON [Assignments].question_id=[Questions].Id
+  group by [Questions].question_type_id
+
+
+  /*Стан - На перевірці, результат - Роз'яснено K6*/
+  IF OBJECT_ID('tempdb..#temp_column6') IS NOT NULL	DROP TABLE #temp_column6;
+
+  select [Questions].question_type_id, convert(numeric(8,2), avg(convert(float,datediff(dd, [Assignments].registration_date, l.Log_Date)))) count_day
+  into #temp_column6
+  from [CRM_1551_Analitics].[dbo].[Assignments]
+  INNER JOIN
+  (select [assignment_id], MIN([Log_Date]) [Log_Date]
+  from [CRM_1551_Analitics].[dbo].[Assignment_History]
+  where [assignment_state_id]=3 /*На перевірці*/ and [AssignmentResultsId]=7 /*Роз`яснено*/
+  group by [assignment_id]) l on [Assignments].Id=l.[assignment_id]
+  INNER JOIN [CRM_1551_Analitics].[dbo].[Questions] ON [Assignments].question_id=[Questions].Id
+  group by [Questions].question_type_id
+
+
+
   SELECT [QuestionTypes].Id,
   [QuestionTypes].Id QuestionTypes_Id, 
   [QuestionTypes].name QuestionTypes_Name, --1
   [Rating_EtalonDaysToExecution].EtalonDaysToExecution, --2
-  CONVERT(NUMERIC(8,2),avg_EtalonDaysToExecution.avg_EtalonDaysToExecution) avg_EtalonDaysToExecution, --3
-  CONVERT(NUMERIC(8,2),avg_EtalonDaysToExecution.avg_EtalonDaysToExecution) avg_EtalonDaysToExecution_change, --4 МОЖЛИВІСТЬ ЗМІНИ
+  --CONVERT(NUMERIC(8,2),avg_EtalonDaysToExecution.avg_EtalonDaysToExecution) avg_EtalonDaysToExecution, --3
+  temp_column3.count_day avg_EtalonDaysToExecution, --3
+  --CONVERT(NUMERIC(8,2),avg_EtalonDaysToExecution.avg_EtalonDaysToExecution) avg_EtalonDaysToExecution_change, --4 МОЖЛИВІСТЬ ЗМІНИ
+  temp_column3.count_day avg_EtalonDaysToExecution_change, --4 МОЖЛИВІСТЬ ЗМІНИ,
   [Rating_EtalonDaysToExecution].[EtalonDaysToExplain], --5
-  CONVERT(NUMERIC(8,2),avg_EtalonDaysToExplain.avg_EtalonDaysToExplain) avg_EtalonDaysToExplain, --6
-  CONVERT(NUMERIC(8,2),avg_EtalonDaysToExplain.avg_EtalonDaysToExplain) avg_EtalonDaysToExplain_change, --7 МОЖЛИВІСТЬ ЗМІНИ
+  --CONVERT(NUMERIC(8,2),avg_EtalonDaysToExplain.avg_EtalonDaysToExplain) avg_EtalonDaysToExplain, --6
+  temp_column6.count_day avg_EtalonDaysToExplain, --6
+  --CONVERT(NUMERIC(8,2),avg_EtalonDaysToExplain.avg_EtalonDaysToExplain) avg_EtalonDaysToExplain_change, --7 МОЖЛИВІСТЬ ЗМІНИ
+  temp_column6.count_day avg_EtalonDaysToExplain_change, --7 МОЖЛИВІСТЬ ЗМІНИ
   [Rating_EtalonDaysToExecution].DateStart --8
   FROM [dbo].[Rating_EtalonDaysToExecution]
   INNER JOIN [CRM_1551_Analitics].[dbo].[QuestionTypes] ON [Rating_EtalonDaysToExecution].QuestionTypeId=[QuestionTypes].Id
 
 
-  LEFT JOIN 
-  (SELECT QuestionTypeId, AVG(CONVERT(FLOAT,EtalonDaysToExecution)) avg_EtalonDaysToExecution
-  FROM [dbo].[Rating_EtalonDaysToExecution]
-  WHERE EtalonDaysToExecution IS NOT NULL AND [Rating_EtalonDaysToExecution].[DateStart] 
-  BETWEEN CONVERT(DATE,@dateFrom) AND CONVERT(DATE,@dateTo)
-  GROUP BY QuestionTypeId) avg_EtalonDaysToExecution ON [Rating_EtalonDaysToExecution].QuestionTypeId=avg_EtalonDaysToExecution.QuestionTypeId
+  --LEFT JOIN 
+  --(SELECT QuestionTypeId, AVG(CONVERT(FLOAT,EtalonDaysToExecution)) avg_EtalonDaysToExecution
+  --FROM [dbo].[Rating_EtalonDaysToExecution]
+  --WHERE EtalonDaysToExecution IS NOT NULL AND [Rating_EtalonDaysToExecution].[DateStart] 
+  --BETWEEN CONVERT(DATE,@dateFrom) AND CONVERT(DATE,@dateTo)
+  --GROUP BY QuestionTypeId) avg_EtalonDaysToExecution ON [Rating_EtalonDaysToExecution].QuestionTypeId=avg_EtalonDaysToExecution.QuestionTypeId
 
   LEFT JOIN 
   (SELECT QuestionTypeId, AVG(CONVERT(FLOAT,EtalonDaysToExplain)) avg_EtalonDaysToExplain
@@ -38,6 +76,9 @@
   WHERE EtalonDaysToExplain IS NOT NULL AND [Rating_EtalonDaysToExecution].[DateStart] 
   BETWEEN CONVERT(DATE,@dateFrom) AND CONVERT(DATE,@dateTo)
   GROUP BY QuestionTypeId) avg_EtalonDaysToExplain ON [Rating_EtalonDaysToExecution].QuestionTypeId=avg_EtalonDaysToExplain.QuestionTypeId
+
+  LEFT JOIN #temp_column3 temp_column3 ON [Rating_EtalonDaysToExecution].QuestionTypeId=temp_column3.question_type_id
+  LEFT JOIN #temp_column6 temp_column6 ON [Rating_EtalonDaysToExecution].QuestionTypeId=temp_column6.question_type_id
 
   WHERE CONVERT(DATE, [Rating_EtalonDaysToExecution].[DateStart])=@date
 
