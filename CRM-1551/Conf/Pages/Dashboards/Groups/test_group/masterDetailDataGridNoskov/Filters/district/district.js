b52aae12d10742a3ed7bbb88277cfb5a3d340a72
Vitalii Noskov <v.a.noskov@outlook.com>
2020-06-01 10:55:51 +0300

    Добавил пример дашборда с мастер деталью.

---
+++ CRM-1551/Conf/Pages/Dashboards/Groups/test_group/masterDetailDataGridNoskov/Filters/district/district.js
@@ -0,0 +1,23 @@
+(function() {
+    return {
+        placeholder: 'Район',
+        keyValue: 'Id',
+        displayValue: 'Name',
+        baseQueryOptions: {
+            queryCode: 'kp_blag_DistrictSelectRows',
+            filterColumns: null,
+            limit: -1,
+            parameterValues: [],
+            pageNumber: 1,
+            sortColumns: [
+                {
+                    key: 'Operator',
+                    value: 0
+                }
+            ]
+        },
+        initValue: function() {
+            this.setDefaultValue('first');
+        }
+    };
+}());
