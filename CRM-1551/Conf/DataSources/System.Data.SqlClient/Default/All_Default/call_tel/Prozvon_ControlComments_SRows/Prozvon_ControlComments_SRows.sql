88a41ca310c563d4ecf106216f34662614da22e5
artemkov1994 <artem_kov@email.ua>
2020-06-15 10:31:44 +0300

    1551_822 убрал постраничность с запроса шаброна

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/call_tel/Prozvon_ControlComments_SRows/Prozvon_ControlComments_SRows.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/call_tel/Prozvon_ControlComments_SRows/Prozvon_ControlComments_SRows.sql
@@ -2,6 +2,6 @@ SELECT Id, [Name]
 FROM [dbo].[ControlComments]
 WHERE [control_type_id]=2
 AND #filter_columns#
-#sort_columns#
+--#sort_columns#
 order by 1
-offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+--offset @pageOffsetRows rows fetch next @pageLimitRows rows only
