89865cca53a26a8a8bac2913b179e2c4a8b685f0
Егор Синьков <egorkatornado@gmail.com>
2020-06-16 15:01:27 +0300

    add excel export

--- CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/processing_appeals/Widgets/subTableNativeDataGridWidget.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/processing_appeals/Widgets/subTableNativeDataGridWidget.js
@@ -41,10 +41,6 @@
                 visible: true,
                 applyFilter: 'auto'
             },
-            export: {
-                enabled: true,
-                fileName: 'Excel'
-            },
             pager: {
                 showPageSizeSelector:  true,
                 allowedPageSizes: [10, 50, 100, 500],
@@ -146,6 +142,18 @@
         },
         createTableButton: function(code, e) {
             let toolbarItems = e.toolbarOptions.items;
+            toolbarItems.push({
+                widget: 'dxButton',
+                location: 'after',
+                options: {
+                    icon: 'exportxlsx',
+                    type: 'default',
+                    text: 'Excel',
+                    onClick: function() {
+                        this.executeExportExcelQuery();
+                    }.bind(this)
+                }
+            });
             if (code === this.arrivedColumn) {
                 toolbarItems.push({
                     widget: 'dxButton',
@@ -161,6 +169,16 @@
                     location: 'after'
                 });
             }
+        },
+        executeExportExcelQuery: function() {
+            this.showPagePreloader('Зачекайте, формується документ');
+            let exportQuery = {
+                queryCode: this.config.query.code,
+                limit: -1,
+                parameterValues: this.config.query.parameterValues
+            }
+            this.queryExecutor(exportQuery, this.createExcelWorkbook, this);
+            this.showPreloader = false;
         },
         findAllSelectedRows: function() {
             const selectedRows = this.dataGridInstance.instance.getSelectedRowsData();
@@ -212,6 +230,158 @@
                     element.appendChild(child);
                 });
             } return element;
+        },
+        createExcelWorkbook: function(data) {
+            const workbook = this.createExcel();
+            const worksheet = workbook.addWorksheet('Заявки', {
+                pageSetup:{
+                    orientation: 'landscape',
+                    fitToPage: false,
+                    margins: {
+                        left: 0.4, right: 0.3,
+                        top: 0.4, bottom: 0.4,
+                        header: 0.0, footer: 0.0
+                    }
+                }
+            });
+            this.excelHeadRowStart = 4;
+            this.excelHeadRowEnd = 4;
+            const columns = this.config.columns;
+            const columnsProperties = [];
+            const rows = [];
+            this.setColumnsProperties(columns, columnsProperties, worksheet);
+            this.setTableHeader(columns, worksheet);
+            this.setWorksheetTitle(worksheet);
+            this.setTableValues(data, worksheet, rows);
+            this.setTableRowsStyles(worksheet, rows);
+            this.helperFunctions.excel.save(workbook, 'Заявки', this.hidePagePreloader);
+        },
+        setColumnsProperties: function(columns, columnsProperties, worksheet) {
+            for (let i = 0; i < columns.length; i++) {
+                const column = columns[i];
+                let header;
+                let index = 0;
+                let width = 19;
+                let columnProp = { header, width, index };
+                if(column.columns) {
+                    for (let j = 0; j < column.columns.length; j++) {
+                        const subColumn = column.columns[j];
+                        columnProp.index += 1;
+                        columnProp.header = subColumn.caption;
+                        columnsProperties.push(columnProp);
+                    }
+                } else {
+                    columnProp.header = column.caption;
+                    columnProp.index += 1;
+                    columnsProperties.push(columnProp);
+                }
+            }
+            worksheet.columns = columnsProperties;
+        },
+        setTableHeader: function(columns, worksheet) {
+            let position = 0;
+            for (let i = 0; i < columns.length; i++) {
+                const column = columns[i];
+                position += 1;
+                worksheet.mergeCells(this.excelHeadRowStart, position, this.excelHeadRowEnd, position);
+                const cell = worksheet.getCell(this.excelHeadRowEnd, position);
+                cell.value = column.caption;
+                this.setCellStyle(cell);
+            }
+            this.lastPosition = position;
+        },
+        setWorksheetTitle: function(worksheet) {
+            worksheet.mergeCells(1, 1, 1, 7);
+            const title = worksheet.getCell(1, 1);
+            title.value = 'Інформація';
+            worksheet.mergeCells(2, 1, 2, 7);
+            const description = worksheet.getCell(2, 1);
+            description.value = 'про звернення громадян, що надійшли до Служби мера'
+        },
+        setTableValues: function(data, worksheet, rowNumbers) {
+            for (let i = 0; i < data.rows.length; i++) {
+                const rowData = data.rows[i];
+                const rowStart = this.excelHeadRowStart + 1 + i;
+                rowNumbers.push(rowStart);
+                for (let j = 1; j < rowData.values.length - 3; j++) {
+                    const value = rowData.values[j];
+                    const cell = worksheet.getCell(rowStart, j);
+                    if (j === 2 || j === 5) {
+                        cell.value = this.changeDateTimeValues(value);
+                    } else {
+                        cell.value = value;
+                    }
+                    this.setCellStyle(cell);
+                }
+            }
+        },
+        setTableRowsStyles: function(worksheet, rowNumbers) {
+            worksheet.getRow(1).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 14,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(1).alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+            worksheet.getRow(2).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 14,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(2).alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+            worksheet.getRow(3).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 10,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(3).alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+            worksheet.getRow(4).font = {
+                name: 'Times New Roman',
+                family: 4,
+                size: 10,
+                underline: false,
+                bold: true,
+                italic: false
+            };
+            worksheet.getRow(4).alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+            worksheet.getRow(1).height = 30;
+            worksheet.getRow(2).height = 30;
+            worksheet.getRow(3).height = 40;
+            worksheet.getRow(4).height = 40;
+            rowNumbers.forEach(number => {
+                worksheet.getRow(number).height = 90;
+                worksheet.getRow(number).font = {
+                    name: 'Times New Roman',
+                    family: 4,
+                    size: 10,
+                    underline: false,
+                    italic: false
+                };
+                worksheet.getRow(number).alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+                worksheet.getCell('A' + number).alignment = { vertical: 'middle', horizontal: 'left', wrapText: true };
+            });
+        },
+        setCellStyle: function(cell) {
+            cell.border = { top: {style:'thin'}, left: {style:'thin'}, bottom: {style:'thin'}, right: {style:'thin'} };
+            cell.alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+            cell.font = { name: 'Times New Roman', family: 4, size: 10, underline: false, bold: false , italic: false };
+        },
+        changeDateTimeValues: function(value) {
+            let date = new Date(value);
+            let dd = date.getDate().toString();
+            let mm = (date.getMonth() + 1).toString();
+            let yyyy = date.getFullYear().toString();
+            dd = dd.length === 1 ? '0' + dd : dd;
+            mm = mm.length === 1 ? '0' + mm : mm;
+            return `${dd}.${mm}.${yyyy}`;
         }
     };
 }());
