87b09d7e8445c0e2301a05f385644e19a993e284
artemkov1994 <artem_kov@email.ua>
2020-07-03 08:38:48 +0300

    1551_851 некоторые запросы и формы согласно задачи

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Dictionarys/EventClass/SelectEventClass/SelectEventClass.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Dictionarys/EventClass/SelectEventClass/SelectEventClass.sql
@@ -1,8 +1,12 @@
 select ec.Id, ec.name, ec.global_id, gt.name as globalType, 
 ec.event_type_id as eventClassTypeId,
 et.name as eventClassTypeName
+,ec.[assignment_class_id]
+,[Assignment_Classes].name [assignment_class_name]
+,[Assignment_Classes].execution_term
 from Event_Class ec
 join EventTypes et on et.id = ec.event_type_id
 left join [CRM_1551_GORODOK_Integrartion].[dbo].[Global_claims_types_new] 
+left join [dbo].[Assignment_Classes] on ec.[assignment_class_id]=[Assignment_Classes].Id
 gt on ec.global_id = gt.id 
 where ec.Id = @Id;
