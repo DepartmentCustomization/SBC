95dd403d33abed156be4bcaf47ffae44e27b1ce1
artemkov1994 <artem_kov@email.ua>
2020-07-17 21:21:57 +0300

    1551_863 деление на 0 при одинаковых датах с и по

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Statistics_on_appeals_from_site/SAFS_MainTable/SAFS_MainTable.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Statistics_on_appeals_from_site/SAFS_MainTable/SAFS_MainTable.sql
@@ -1,14 +1,16 @@
---declare @date_from date='2020-06-01',
---  @date_to date='2020-07-29';
 
+/*
+declare @date_from date='2020-07-29',
+  @date_to date='2020-07-29';
+  */
 
-  select 1 Id, ltrim(count(Id))+N' ('+ltrim(convert(numeric(8,2),convert(float,count(Id))/convert(float,(datediff(dd, convert(date, @date_from), convert(date, @date_to))))))+N')' cell1,
-
+  select 1 Id, ltrim(count(Id))+N' ('+ltrim(convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to))))))+N')' cell1,
+  
   (select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=dateadd(dd, -1, getutcdate())) cell2,
 
   
   convert(numeric(8,2),(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=dateadd(dd, -1, getutcdate())))-
-  convert(numeric(8,2),convert(float,count(Id))/convert(float,(datediff(dd, convert(date, @date_from), convert(date, @date_to)))))
+  convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to)))))
   cell2_delta,
 
   ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=getutcdate() and [AppealFromSiteResultId]=1))+
@@ -22,6 +24,6 @@
   and date between convert(date, @date_from) and convert(date, @date_to)) cell4,
 
   (select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate) between convert(date, @date_from) and convert(date, @date_to) and [AppealFromSiteResultId]=2) cell5
-
+ /* */
   from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite]
   where convert(date, ReceiptDate) between convert(date, @date_from) and convert(date, @date_to)
