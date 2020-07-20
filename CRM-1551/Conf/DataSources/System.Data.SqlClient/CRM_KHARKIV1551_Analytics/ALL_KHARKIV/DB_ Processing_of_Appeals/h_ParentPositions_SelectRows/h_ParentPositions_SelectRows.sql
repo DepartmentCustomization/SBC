942291dbcd1a7e46064af091eeb10594bcdbc2f9
artemkov1994 <artem_kov@email.ua>
2020-07-20 14:48:43 +0300

    1551_827 изменил запрос на выпадаючий список

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_KHARKIV1551_Analytics/ALL_KHARKIV/DB_ Processing_of_Appeals/h_ParentPositions_SelectRows/h_ParentPositions_SelectRows.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_KHARKIV1551_Analytics/ALL_KHARKIV/DB_ Processing_of_Appeals/h_ParentPositions_SelectRows/h_ParentPositions_SelectRows.sql
@@ -1,10 +1,45 @@
-select [Positions_parent].Id, 
-[Positions].organizations_id vykonavets_Id,
-ISNULL([Positions_parent].position, N'')+ ISNULL(N' ('+[Organizations].short_name+N')',N'') name
-  from [dbo].[Positions] 
-  inner join [dbo].[Positions] [Positions_parent] on [Positions].parent_id=[Positions_parent].Id
-  left join [dbo].[Organizations] on [Positions_parent].organizations_id=[Organizations].Id
-  where  #filter_columns#
-  --#sort_columns#
-  order by 1
- --offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+
+if object_id('tempdb..#org_and_parent') is not null 
+begin
+drop table #org_and_parent
+end
+
+
+;with
+pit as -- родители @id
+(
+select Id, [parent_organization_id] ParentId, short_name name
+from [dbo].[Organizations]  t
+union all
+select t.Id, t.[parent_organization_id] ParentId, t.short_name name
+from [dbo].[Organizations]  t inner join pit on t.Id=pit.ParentId
+)
+select distinct * into #org_and_parent from pit-- pit it
+
+
+-- вывести:
+/*
+1.организацию исполнителя
+2.их родительские организации
+3.главные посади родительских организаций
+*/
+
+if object_id('tempdb..#position_org') is not null 
+begin
+drop table #position_org
+end
+
+select [Positions].Id, [Positions].organizations_id, [Positions].Id position_id, ISNULL([Positions].position+N' ',N'')+ISNULL(N'('+[Organizations].short_name+N')',N'') name
+into #position_org
+from [dbo].[Positions]
+left join [dbo].[Organizations] on [Positions].organizations_id=[Organizations].Id
+where [Positions].is_main='true'
+
+
+--select * from #org_and_parent
+
+select #position_org.Id, #org_and_parent.Id vykonavets_Id, #position_org.name
+from #position_org
+inner join #org_and_parent on #position_org.organizations_id=#org_and_parent.ParentId
+
+
