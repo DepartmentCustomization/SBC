40998840d94fd465a8d5158d69a6e190e6361032
GrigoraschenkoN <63046795+GrigoraschenkoN@users.noreply.github.com>
2020-07-23 17:15:15 +0300

    update

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_SelectRow/Polls_SelectRow.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_SelectRow/Polls_SelectRow.sql
@@ -2,7 +2,7 @@ select poll_name, is_active, start_date, end_date, PollDirection.name,  AllAppli
 from Polls
 inner join Polls_PollDirections on Polls_PollDirections.poll_id = Polls.id
 inner join PollDirection on PollDirection.id = Polls_PollDirections.direction_id
-inner join ( select poll_id, count(id) as col_Applicants from PollsApplicants  group by poll_id) AllApplicants on AllApplicants.poll_id = Polls.id
-inner join ( select poll_id, count(id) as col_IsPollsApplicants from PollsApplicants  where poll_date is not null group by poll_id) IsPollsApplicants on IsPollsApplicants.poll_id = Polls.id
-inner join ( select poll_id, count(id) as col_IsNotApplicants from PollsApplicants  where reject_poll = 1 group by poll_id) IsNotPollsApplicants on IsNotPollsApplicants.poll_id = Polls.id
+left join ( select poll_id, count(id) as col_Applicants from PollsApplicants  group by poll_id) AllApplicants on AllApplicants.poll_id = Polls.id
+left join ( select poll_id, count(id) as col_IsPollsApplicants from PollsApplicants  where poll_date is not null group by poll_id) IsPollsApplicants on IsPollsApplicants.poll_id = Polls.id
+left join ( select poll_id, count(id) as col_IsNotApplicants from PollsApplicants  where reject_poll = 1 group by poll_id) IsNotPollsApplicants on IsNotPollsApplicants.poll_id = Polls.id
 where Polls.Id = @Id
