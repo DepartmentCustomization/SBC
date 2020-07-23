06208709d4ac203d21266cbb6099a054d1c9d01b
GrigoraschenkoN <63046795+GrigoraschenkoN@users.noreply.github.com>
2020-07-23 14:54:41 +0300

    update

---
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_SelectRows/Polls_SelectRows.sql
@@ -0,0 +1,11 @@
+select poll_name, is_active, start_date, end_date, PollDirection.name,  AllApplicants.col_Applicants, IsPollsApplicants.col_IsPollsApplicants, IsNotPollsApplicants.col_IsNotApplicants
+from Polls
+inner join Polls_PollDirections on Polls_PollDirections.poll_id = Polls.id
+inner join PollDirection on PollDirection.id = Polls_PollDirections.direction_id
+inner join ( select poll_id, count(id) as col_Applicants from PollsApplicants  group by poll_id) AllApplicants on AllApplicants.poll_id = Polls.id
+inner join ( select poll_id, count(id) as col_IsPollsApplicants from PollsApplicants  where poll_date is not null group by poll_id) IsPollsApplicants on IsPollsApplicants.poll_id = Polls.id
+inner join ( select poll_id, count(id) as col_IsNotApplicants from PollsApplicants  where reject_poll = 1 group by poll_id) IsNotPollsApplicants on IsNotPollsApplicants.poll_id = Polls.id
+where cast(start_date as date) >= cast(@DateStart as date)and cast(end_date as date)<= cast(@DateEnd as date)
+and #filter_columns#
+#sort_columns#
+offset @pageOffsetRows rows fetch next @pageLimitRows rows only  
