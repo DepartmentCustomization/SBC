65eb53030828d883f786fc45217f9d78501fb482
GrigoraschenkoN <63046795+GrigoraschenkoN@users.noreply.github.com>
2020-07-24 14:51:41 +0300

    update

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_SelectRows/Polls_SelectRows.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_SelectRows/Polls_SelectRows.sql
@@ -1,4 +1,4 @@
-select poll_name, is_active, start_date, end_date, PollDirection.name,  AllApplicants.col_Applicants, IsPollsApplicants.col_IsPollsApplicants, IsNotPollsApplicants.col_IsNotApplicants
+select poll_name, is_active, start_date, end_date, PollDirection.id as PollDirId, PollDirection.name,  AllApplicants.col_Applicants, IsPollsApplicants.col_IsPollsApplicants, IsNotPollsApplicants.col_IsNotApplicants
 from Polls
 inner join Polls_PollDirections on Polls_PollDirections.poll_id = Polls.id
 inner join PollDirection on PollDirection.id = Polls_PollDirections.direction_id
@@ -7,5 +7,5 @@ left join ( select poll_id, count(id) as col_IsPollsApplicants from PollsApplica
 left join ( select poll_id, count(id) as col_IsNotApplicants from PollsApplicants  where reject_poll = 1 group by poll_id) IsNotPollsApplicants on IsNotPollsApplicants.poll_id = Polls.id
 where cast([start_date] as date) >= cast(@DateStart as date)  and cast(end_date as date) <= cast(@DateEnd as date) and
 (is_active  = @is_active  or isnull(@is_active, 2) = 2) and
-#filter_columns#
+#filter_columns# 
 
