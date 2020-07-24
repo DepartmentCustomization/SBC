4bab3c4d975aef7ea82050c557f5bdab4d844634
GrigoraschenkoN <63046795+GrigoraschenkoN@users.noreply.github.com>
2020-07-24 11:24:53 +0300

    update

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_SelectRows/Polls_SelectRows.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_SelectRows/Polls_SelectRows.sql
@@ -6,6 +6,7 @@ left join ( select poll_id, count(id) as col_Applicants from PollsApplicants  gr
 left join ( select poll_id, count(id) as col_IsPollsApplicants from PollsApplicants  where poll_date is not null group by poll_id) IsPollsApplicants on IsPollsApplicants.poll_id = Polls.id
 left join ( select poll_id, count(id) as col_IsNotApplicants from PollsApplicants  where reject_poll = 1 group by poll_id) IsNotPollsApplicants on IsNotPollsApplicants.poll_id = Polls.id
 where cast([start_date] as date) >= cast(@DateStart as date)  and cast(end_date as date) <= cast(@DateEnd as date) and
+(is_active  = @is_active  or isnull(@is_active, 0) = 0) and
 #filter_columns#
 order by 1
 offset @pageOffsetRows rows fetch next @pageLimitRows rows only
