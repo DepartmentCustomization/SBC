661dc29d4b6e25b8b61e5f76724f139b84f6e863
Виталий Стародуб <v.starodub@domain.local>
2020-07-29 11:19:00 +0300

    a

--- CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/poll_settings/Widgets/CustomWidget-0.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/poll_settings/Widgets/CustomWidget-0.js
@@ -1,19 +1,37 @@
 (function() {
     return {
-        title: '',
+        title: 'Опитування',
         hint: '',
         formatTitle: function() {},
         customConfig:
                     `
+                    <div class='interview-container'>
+                        <div class='static' id='static'></div>
+                    </div>
                     `
         ,
         init: function() {
-            let executeQuery = {
-                queryCode: '',
-                limit: -1,
-                parameterValues: []
-            };
-            this.queryExecutor(executeQuery, this.load, this);
+        },
+        afterViewInit() {
+            this.createStatic()
+        },
+        createElement: function(tag, props, ...children) {
+            const element = document.createElement(tag);
+            Object.keys(props).forEach(key => element[key] = props[key]);
+            if(children.length > 0) {
+                children.forEach(child =>{
+                    element.appendChild(child);
+                });
+            } return element;
+        },
+        createStatic() {
+            const conStatic = document.getElementById('static');
+            const interviewName = this.createElement('input',{className:'interview-name',placeholder:'Назва опитування',required:'true'});
+            const buttonsBlock = this.createElement('div', {className: 'main-buttons-block'});
+            const buttonBack = this.createElement('button', {className: 'main-button-back',textContent: 'Назад', id: 'main-button-back'});
+            const buttonSave = this.createElement('button', {className: 'main-button-save',textContent: 'Назад', id: 'main-button-save'});
+            buttonsBlock.append(buttonBack,buttonSave);
+            conStatic.append(interviewName,buttonsBlock);
         },
         load: function() {
         }
