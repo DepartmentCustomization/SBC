68a406f2b22c332e298bcf59a7af732c9b8da323
artemkov1994 <artem_kov@email.ua>
2020-07-31 11:00:23 +0300

    1551_835 выпадающий список на резолюцию

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_KHARKIV1551_Analytics/ALL_KHARKIV/Job_Applications/h_JA_ResolutionsSRows/h_JA_ResolutionsSRows.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_KHARKIV1551_Analytics/ALL_KHARKIV/Job_Applications/h_JA_ResolutionsSRows/h_JA_ResolutionsSRows.sql
@@ -1,4 +1,5 @@
-select distinct [AssignmentResolutions].Id, [new_assignment_result_id] result_id, [AssignmentResolutions].Name
+
+select distinct [AssignmentResolutions].Id, [AssignmentResolutions].Id resolution_id, [new_assignment_result_id] result_id, [AssignmentResolutions].Name resolution_name
   from [CRM_1551_Analitics].[dbo].[TransitionAssignmentStates]
   inner join [CRM_1551_Analitics].[dbo].[AssignmentResolutions] on [TransitionAssignmentStates].new_assignment_resolution_id=[AssignmentResolutions].Id
   where #filter_columns#
