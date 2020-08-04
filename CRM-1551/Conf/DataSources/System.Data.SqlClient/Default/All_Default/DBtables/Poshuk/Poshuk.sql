6192ee94058c1ec008191505d5bc9ee2d4f5e05b
MarkusEfre <markus.efr@gmail.com>
2020-08-04 13:09:56 +0300

    Task 884 - п.2.1 инфа в мастер детали с поиска

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/DBtables/Poshuk/Poshuk.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/DBtables/Poshuk/Poshuk.sql
@@ -100,7 +100,10 @@ SELECT
 	applicant.[full_name] AS zayavnyk,
 	appeal_object.[name] AS adress,
 	execute_org.[short_name] AS vykonavets,
-	question.[control_date]
+	question.[control_date],
+	applicant.[ApplicantAdress] AS zayavnyk_adress,
+	question.[question_content] AS zayavnyk_zmist,
+	balans_org.[short_name] AS balans_name
 FROM '+@Archive+N'[dbo].[Appeals] appeal
 INNER JOIN '+@Archive+N'[dbo].[Questions] question ON appeal.Id = question.appeal_id
 INNER JOIN '+@Archive+N'[dbo].[Assignments] assignment ON assignment.question_id = question.Id
@@ -110,6 +113,10 @@ LEFT JOIN [dbo].[AssignmentStates] state ON state.Id = assignment.assignment_sta
 LEFT JOIN [dbo].[Applicants] applicant ON applicant.Id = appeal.applicant_id
 LEFT JOIN [dbo].[Objects] appeal_object ON appeal_object.Id = question.object_id 
 LEFT JOIN [dbo].[Organizations] execute_org ON execute_org.Id = assignment.executor_organization_id
+LEFT JOIN [dbo].[Buildings] building ON building.Id = appeal_object.builbing_id
+LEFT JOIN [dbo].[ExecutorInRoleForObject] balans ON balans.building_id = building.Id
+	AND [executor_role_id]=1
+LEFT JOIN [dbo].[Organizations] balans_org ON balans_org.Id = balans.executor_id
 WHERE appeal.[registration_number] IN (SELECT 
 										 [Num]
 									 FROM @Numstab)
