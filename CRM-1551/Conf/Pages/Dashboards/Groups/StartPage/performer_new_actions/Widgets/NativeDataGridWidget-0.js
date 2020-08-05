097dc757608f848e19d93295b4f3634df8e1c20e
Егор Синьков <egorkatornado@gmail.com>
2020-08-05 14:31:35 +0300

    upd object

--- CRM-1551/Conf/Pages/Dashboards/Groups/StartPage/performer_new_actions/Widgets/NativeDataGridWidget-0.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/StartPage/performer_new_actions/Widgets/NativeDataGridWidget-0.js
@@ -220,7 +220,8 @@
             } else {
                 document.getElementById('table_events').style.display = 'block';
                 this.config.query.queryCode = 'EventsTable';
-                this.config.query.parameterValues = [{ key: '@organization_id', typeEvent: message.orgId },
+                this.config.query.parameterValues = [
+                    { key: '@organization_id', value: message.orgId },
                     { key: '@OtKuda', value: source },
                     { key: '@TypeEvent', value: typeEvent }
                 ];
