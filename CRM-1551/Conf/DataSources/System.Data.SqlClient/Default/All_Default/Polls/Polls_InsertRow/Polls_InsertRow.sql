74c245d5bed9458417ba886d69768a4a457ef608
GrigoraschenkoN <63046795+GrigoraschenkoN@users.noreply.github.com>
2020-08-07 17:37:41 +0300

    update

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_InsertRow/Polls_InsertRow.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Polls/Polls_InsertRow/Polls_InsertRow.sql
@@ -24,8 +24,21 @@ values ( @poll_name
 declare @app_id int
 set @app_id = (select top 1 Id from @output)
 
-select @app_id as [Id]
-return;
+--select @app_id as [Id]
+--return;
 
-insert into [dbo].Polls_PollDirections (poll_id, direction_id, user_id, edit_date, user_edit_id )
+declare @output2 table (Id int)
+Insert into [dbo].Polls_PollDirections (poll_id
+							,direction_id
+							,user_id
+							,edit_date
+							,user_edit_id							
+							)
+output [inserted].[Id] into @output2 (Id)
 values (@app_id,  @direction_id, @CreatedUserById, GETUTCDATE(), @CreatedUserById)
+
+declare @app_id2 int
+set @app_id2 = (select top 1 Id from @output2)
+
+select @app_id2 as [Id]
+return;
