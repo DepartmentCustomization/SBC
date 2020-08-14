d46fe91c831b1442b9ddfeae6f0a3b18954b0b0c
MarkusEfre <markus.efr@gmail.com>
2020-08-14 16:02:11 +0300

    Task 920 - done

--- CRM-1551/Conf/Forms/PersonExecutorChoose/PersonExecutorChoose.form.js
+++ CRM-1551/Conf/Forms/PersonExecutorChoose/PersonExecutorChoose.form.js
@@ -1,20 +1,21 @@
 (function() {
     return {
         init: function() {
-            this.form.onControlValueChanged('organization_id', this.onStreetsChanged);
+            this.form.onControlValueChanged('organization_id', this.organizationChanged.bind(this));
+            let organization_id = this.form.getControlValue('organization_id');
+            let orgParam = [{ parameterCode: '@organization_id', parameterValue: organization_id }];
+            this.form.setControlParameterValues('position_id', orgParam);
         },
-        onStreetsChanged: function(dis_id) {
-            if (typeof dis_id === 'string') {
-                return
-            } else if (dis_id == null) {
-                this.form.setControlValue('position_id', null);
-                let dependParams = [{ parameterCode: '@organization_id', parameterValue: dis_id }];
-                this.form.setControlParameterValues('position_id', dependParams);
-                this.form.disableControl('position_id');
-            } else {
-                let dependParams = [{ parameterCode: '@organization_id', parameterValue: dis_id }];
+        organizationChanged: function(val) {
+            if (typeof val === 'number') {
+                let dependParams = [{ parameterCode: '@organization_id', parameterValue: val }];
                 this.form.setControlParameterValues('position_id', dependParams);
                 this.form.enableControl('position_id');
+            } else if (val === null) {
+                this.form.setControlValue('position_id', null);
+                let dependParams = [{ parameterCode: '@organization_id', parameterValue: val }];
+                this.form.setControlParameterValues('position_id', dependParams);
+                this.form.disableControl('position_id');
             }
         }
     };
