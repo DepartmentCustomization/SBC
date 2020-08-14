ccc878b4460dd6cdc5f4ed9951dbbc0caa9b9d64
artemkov1994 <artem_kov@email.ua>
2020-08-14 10:21:24 +0300

    1551_916 добавил отобращение для 1 и 1761

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/DynamicsRatingRDA/RDA_SELECTROWS/RDA_SELECTROWS.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/CRM_1551_Rating/RootGroup_CRM_1551_Rating/DynamicsRatingRDA/RDA_SELECTROWS/RDA_SELECTROWS.sql
@@ -1,5 +1,5 @@
 
---declare @user_id nvarchar(128)=N'36d11ab9-4151-4b63-8cd7-fb5008996af4'
+--declare @user_id nvarchar(128)=N'36d11ab9-4151-4b63-8cd7-fb5008996af4'--N'29796543-b903-48a6-9399-4840f6eac396'--
 
 --select Id, short_name Name
 --  from [CRM_1551_Analitics].[dbo].[Organizations]
@@ -25,7 +25,12 @@ from
   inner join [CRM_1551_Analitics].[dbo].[Positions] [Positions2] on [PositionsHelpers].main_position_id=[Positions2].Id
   inner join [CRM_1551_Analitics].[dbo].[Organizations] on [Positions2].organizations_id=[Organizations].id
   where [Positions].[programuser_id]=@user_id and [Organizations].organization_type_id=5/*РДА*/
+  union
+  select [Organizations].Id, [Organizations].short_name Name
+  from [CRM_1551_Analitics].[dbo].[Organizations]
+  where [Organizations].organization_type_id=5/*РДА*/
+  and ((select organizations_id from [CRM_1551_Analitics].[dbo].[Positions] where programuser_id=@user_id) in (1,1761))
   ) t
-  where #filter_columns#
+  where id<>3 and #filter_columns#
     ORDER BY 1
  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
