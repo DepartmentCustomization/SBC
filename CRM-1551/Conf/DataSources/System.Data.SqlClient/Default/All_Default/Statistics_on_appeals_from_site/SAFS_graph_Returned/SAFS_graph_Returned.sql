7b2d8080d27f14c2d9e3cf27e1683f6a7d19797e
artemkov1994 <artem_kov@email.ua>
2020-09-01 15:42:41 +0300

    1551_918 name добавил

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Statistics_on_appeals_from_site/SAFS_graph_Returned/SAFS_graph_Returned.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Statistics_on_appeals_from_site/SAFS_graph_Returned/SAFS_graph_Returned.sql
@@ -1,3 +1,4 @@
+
 declare @date_table table (Id int identity(1,1), date date)
   declare @date_f date=convert(date, @date_from)
 
@@ -10,6 +11,58 @@ declare @date_table table (Id int identity(1,1), date date)
 
   end
 
-  select date_table.Id, date_table.date, [indicator_value]
-  from @date_table date_table left join
-  [CRM_1551_Site_Integration].[dbo].[Statistic] on date_table.date=[Statistic].date and [Statistic].diagram=9
+  --select * from @date_table
+
+  if datediff(dd, @date_from, @date_to)<=30
+
+    begin
+	    select date_table.Id, date_table.date, isnull([indicator_value],0) [indicator_value], ltrim(date_table.date) name
+	    from @date_table date_table left join
+	    [CRM_1551_Site_Integration].[dbo].[Statistic] on date_table.date=[Statistic].date and [Statistic].diagram=9
+    end
+
+  if datediff(dd, @date_from, @date_to) between 31 and 90
+
+	begin
+		select --date_table.Id, 
+		datepart(ww, date_table.date) Id,
+		min(date_table.date) date, 
+		sum(isnull([indicator_value],0)) [indicator_value],
+		ltrim(min(date_table.date)) name
+	    from @date_table date_table left join
+	    [CRM_1551_Site_Integration].[dbo].[Statistic] on date_table.date=[Statistic].date and [Statistic].diagram=9
+		group by datepart(ww, date_table.date)
+	end
+
+  if datediff(dd, @date_from, @date_to) >90
+
+	begin
+		select --date_table.Id, 
+		(ltrim(datepart(yy, date_table.date))+ltrim(datepart(mm, date_table.date)))*1 Id,
+		min(date_table.date) date, 
+		sum(isnull([indicator_value],0)) [indicator_value],
+		datename(mm,min(date_table.date))+N' '+datename(yy,min(date_table.date)) name
+		--year(min(date_table.date))
+	    from @date_table date_table left join
+	    [CRM_1551_Site_Integration].[dbo].[Statistic] on date_table.date=[Statistic].date and [Statistic].diagram=9
+		group by datepart(yy, date_table.date), datepart(mm, date_table.date)
+		order by min(date_table.date)
+	end
+
+
+
+-- declare @date_table table (Id int identity(1,1), date date)
+--   declare @date_f date=convert(date, @date_from)
+
+--   while @date_f<=convert(date, @date_to)
+--   begin 
+--   insert into @date_table (date)
+--   select @date_f
+
+--   set @date_f=dateadd(dd, 1, @date_f)
+
+--   end
+
+--   select date_table.Id, date_table.date, [indicator_value]
+--   from @date_table date_table left join
+--   [CRM_1551_Site_Integration].[dbo].[Statistic] on date_table.date=[Statistic].date and [Statistic].diagram=9
