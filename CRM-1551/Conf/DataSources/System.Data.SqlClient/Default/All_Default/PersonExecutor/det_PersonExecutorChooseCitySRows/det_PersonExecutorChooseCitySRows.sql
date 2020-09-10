ef7cc56c6f8a3e79c0f500c4c0019ab2971e35d9
artemkov1994 <artem_kov@email.ua>
2020-09-10 14:16:22 +0300

    1551_911 добавил формы на район и город и по коментариям

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/PersonExecutor/det_PersonExecutorChooseCitySRows/det_PersonExecutorChooseCitySRows.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/PersonExecutor/det_PersonExecutorChooseCitySRows/det_PersonExecutorChooseCitySRows.sql
@@ -1,7 +1,12 @@
-select [PersonExecutorChoose].Id, [City].name
-  from [dbo].[City], [dbo].[PersonExecutorChoose]
-  where [PersonExecutorChoose].Id=@person_executor_choose_id and
-  #filter_columns#
+select [PersonExecutorChoose].Id, 
+  --[PersonExecutorChoose].Id person_executor_choose_id, [PersonExecutorChoose].name person_executor_choose_name,
+  --[PersonExecutorChooseObjects].city_id, 
+  [PersonExecutorChooseObjects].city_name
+  from [dbo].[PersonExecutorChoose]
+  inner join (select distinct person_executor_choose_id, (select Id from [dbo].[City]) city_id, (select name from [dbo].[City]) city_name
+  from [dbo].[PersonExecutorChooseObjects] where object_id is not null) [PersonExecutorChooseObjects] on [PersonExecutorChoose].Id=[PersonExecutorChooseObjects].person_executor_choose_id
+where [PersonExecutorChoose].Id=@person_executor_choose_id
+  and #filter_columns#
   --#sort_columns#
   order by 1
  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
