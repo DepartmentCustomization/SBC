fb974857d5acc107c3d08044b14f3693d276a210
MarkusEfre <markus.efr@gmail.com>
2020-09-14 10:27:59 +0300

    914 - проверка edit_date на "Розподілити"

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Button_Nadiishlo_Rozpodility/Button_Nadiishlo_Rozpodility.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Button_Nadiishlo_Rozpodility/Button_Nadiishlo_Rozpodility.sql
@@ -1,67 +1,105 @@
-declare @output_con table (Id int);
-declare @new_con int;
-declare @result_id int;
-declare @resolution_id int;
-declare @current_consid int;
-declare @ass_state_id  int
-	select @ass_state_id  = assignment_state_id
-	, @result_id = AssignmentResultsId
-	, @resolution_id = AssignmentResolutionsId
-	, @current_consid = current_assignment_consideration_id 
-	from Assignments where Id = @Id
+DECLARE @output_con TABLE (Id INT);
+DECLARE @new_con INT;
+DECLARE @result_id INT;
+DECLARE @resolution_id INT;
+DECLARE @current_consid INT;
+DECLARE @ass_state_id INT;
+SELECT
+	@ass_state_id = assignment_state_id,
+	@result_id = AssignmentResultsId,
+	@resolution_id = AssignmentResolutionsId,
+	@current_consid = current_assignment_consideration_id
+FROM
+	dbo.Assignments
+WHERE
+	Id = @Id;
 
+IF(
+	@current_edit_date <> (
+		SELECT
+			edit_date
+		FROM
+			dbo.[Assignments]
+		WHERE
+			Id = @Id
+	)
+) 
+BEGIN 
+	RAISERROR(
+	N'У дорученні відбулися зміни, оновіть сторінку',
+	16,
+	1
+);
+RETURN;
+END
+ELSE 
+BEGIN
 -- 	if @result_id = 1 and @resolution_id is null and @executor_organization_id is not null
-	if  @executor_organization_id is not null
-	begin
-    -- 	if @executor_organization_id <> (select organization_id from Assignments where Id = @Id)
-    -- 	begin
-    		
-    		UPDATE  [dbo].[Assignments]
-    			  set   --[assignment_state_id]= @ass_state_id 
-    				    [executor_organization_id]= @executor_organization_id -- новый исполнитель на кого переопределили  
-    				   ,[edit_date]= getutcdate()
-    				   ,[user_edit_id]= @user_edit_id
-					  ,[LogUpdated_Query] = N'Button_Nadiishlo_Rozpodility_Row24'
-    				   --,[AssignmentResultsId] = @result_id
-    				   --, AssignmentResolutionsId= @resolution_id
-    		WHERE Id= @Id
-    	
-    		update dbo.AssignmentConsiderations
-    				set	
-    				    [edit_date] = getutcdate()
-    				   ,[user_edit_id] = @user_edit_id
-    				   ,[consideration_date] = getutcdate()
-    		where Id = @current_consid 
-    
-    		delete from @output_con
-    		insert into dbo.AssignmentConsiderations
-    			(		[assignment_id]
-    				   ,[consideration_date]
-    				   ,[assignment_result_id]
-    				   ,[assignment_resolution_id]
-    				   ,[user_id]
-    				   ,[edit_date]
-    				   ,[user_edit_id]
-    				   ,[first_executor_organization_id]
-    				   ,create_date
-    				   ,transfer_date
-    			)
-    			output inserted.Id into @output_con([Id])
-    			values( @Id
-    				    ,GETUTCDATE()
-    				    ,@result_id
-    				    ,@resolution_id
-    				    ,@user_edit_id
-    				    ,GETUTCDATE()
-    				    ,@user_edit_id
-    				    ,@executor_organization_id  -- новый исполнитель на кого переопределили
-    				    ,GETUTCDATE()
-    				    ,GETUTCDATE()
-    				   )
-    
-    		set @new_con = (select top (1) Id from @output_con)
-    		update [Assignments] set current_assignment_consideration_id = @new_con 
-			,[edit_date]=GETUTCDATE()
-			where Id = @Id
-    -- 	end
-	end
+IF @executor_organization_id IS NOT NULL 
+BEGIN 
+-- 	if @executor_organization_id <> (select organization_id from Assignments where Id = @Id)
+-- 	begin
+UPDATE
+	[dbo].[Assignments]
+SET
+	--[assignment_state_id]= @ass_state_id 
+	[executor_organization_id] = @executor_organization_id, -- новый исполнитель на кого переопределили  
+	[edit_date] = getutcdate(),
+	[user_edit_id] = @user_edit_id,
+	[LogUpdated_Query] = N'Button_Nadiishlo_Rozpodility_Row24' --,[AssignmentResultsId] = @result_id
+	--, AssignmentResolutionsId= @resolution_id
+WHERE
+	Id = @Id;
+UPDATE
+	dbo.AssignmentConsiderations
+SET
+	[edit_date] = getutcdate(),
+	[user_edit_id] = @user_edit_id,
+	[consideration_date] = getutcdate()
+WHERE
+	Id = @current_consid;
+DELETE FROM
+	@output_con;
+INSERT INTO
+	dbo.AssignmentConsiderations (
+		[assignment_id],
+		[consideration_date],
+		[assignment_result_id],
+		[assignment_resolution_id],
+		[user_id],
+		[edit_date],
+		[user_edit_id],
+		[first_executor_organization_id],
+		create_date,
+		transfer_date
+	) output inserted.Id INTO @output_con([Id])
+VALUES
+(
+		@Id,
+		GETUTCDATE(),
+		@result_id,
+		@resolution_id,
+		@user_edit_id,
+		GETUTCDATE(),
+		@user_edit_id,
+		@executor_organization_id -- новый исполнитель на кого переопределили
+,
+		GETUTCDATE(),
+		GETUTCDATE()
+	);
+SET
+	@new_con = (
+		SELECT
+			TOP (1) Id
+		FROM
+			@output_con
+	);
+UPDATE
+	dbo.[Assignments]
+SET
+	current_assignment_consideration_id = @new_con,
+	[edit_date] = GETUTCDATE()
+WHERE
+	Id = @Id; -- 	end
+END
+END
