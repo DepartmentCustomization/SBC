714d7318fcd1bf41ad4b931da3297fcf1adcacb9
MarkusEfre <markus.efr@gmail.com>
2020-09-21 11:42:26 +0300

    388 - part 1 правка запросов

--- CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Organizations_WO/All_contacts/avr_Contacts_WS_SelectRows/avr_Contacts_WS_SelectRows.sql
+++ CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Organizations_WO/All_contacts/avr_Contacts_WS_SelectRows/avr_Contacts_WS_SelectRows.sql
@@ -1,26 +1,40 @@
-SELECT [Contacts].[Id]
-      ,Contact_types.Name as contact_type_name
-	  ,Organizations.Name as organizations_name
-	  ,Organizations.Id as organizations_id
-	  ,Contacts.Name as contacts_name
-	  ,cast (tabl_phone.phone_number as nvarchar) as Number
-      ,Jobs.Job_name as jobs_name
-	  ,Positions.Name as positions_name
-	  ,case when Jobs.is_work = 1 then 'Так'
-	            when Jobs.is_work = 0 then 'Ні' 
-	   end is_work
-  FROM [dbo].[Contacts]
-	left join Contact_types on Contact_types.Id = Contacts.Contact_type_ID
-	left join Organizations on Organizations.Id = Contacts.Organisation_ID
--- 	left join Jobs on Jobs.Id = Contacts.Job_ID
-	left join Jobs on Jobs.Contacts_ID = Contacts.external_Id
-	left join Positions on Positions.Id = Jobs.Position_ID
-	left join (SELECT Contact_ID
-					,(select rtrim(a.Number) +N';' as 'data()' from dbo.Contact_phones as a where b.Contact_ID = a.Contact_ID for xml path('') 
-					) as phone_number
-			FROM dbo.Contact_phones b GROUP BY Contact_ID) as tabl_phone on tabl_phone.Contact_ID = Contacts.Id 
-	where Contacts.Job_ID is not null
-	and 
-	#filter_columns#
-     #sort_columns#
- offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+SELECT
+	[Contacts].[Id],
+	Contact_types.Name AS contact_type_name,
+	Organizations.Name AS organizations_name,
+	Organizations.Id AS organizations_id,
+	Contacts.Name AS contacts_name,
+	cast(tabl_phone.phone_number AS NVARCHAR(12)) AS Number,
+	Jobs.Job_name AS jobs_name,
+	Positions.Name AS positions_name,
+CASE
+		WHEN Jobs.is_work = 1 THEN 'Так'
+		WHEN Jobs.is_work = 0 THEN 'Ні'
+	END is_work
+FROM
+	[dbo].[Contacts]
+	LEFT JOIN Contact_types ON Contact_types.Id = Contacts.Contact_type_ID
+	LEFT JOIN Organizations ON Organizations.Id = Contacts.Organisation_ID 
+	LEFT JOIN Jobs ON Jobs.Contacts_ID = Contacts.Id
+	LEFT JOIN Positions ON Positions.Id = Jobs.Position_ID
+	LEFT JOIN (
+		SELECT
+			Contact_ID,
+(
+				SELECT
+					rtrim(a.Number) + N';' AS 'data()'
+				FROM
+					dbo.Contact_phones AS a
+				WHERE
+					b.Contact_ID = a.Contact_ID FOR XML PATH('')
+			) AS phone_number
+		FROM
+			dbo.Contact_phones b
+		GROUP BY
+			Contact_ID
+	) AS tabl_phone ON tabl_phone.Contact_ID = Contacts.Id
+WHERE
+	Contacts.Job_ID IS NOT NULL
+	AND #filter_columns#
+		#sort_columns#
+	OFFSET @pageOffsetRows ROWS FETCH next @pageLimitRows ROWS ONLY;
