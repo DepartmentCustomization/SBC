4569b4962caabb909b336fb168af54601fc6962d
Виталий Стародуб <v.starodub@domain.local>
2020-09-24 14:58:56 +0300

    add new logic

--- CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/essence_on_control/Widgets/CustomWidget-0.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/essence_on_control/Widgets/CustomWidget-0.js
@@ -8,31 +8,55 @@
                     <div class='main-buttons-block' id='main-buttons-block'></div>
                     `
         ,
-        afterViewInit: function() {
-            this.createButtons()
+        init() {
+            const getButtonsValues = {
+                queryCode: 'essence_group_SelectRows',
+                limit: -1,
+                parameterValues: [
+                ]
+            };
+            this.queryExecutor(getButtonsValues,this.createButtons,this);
         },
-        createButtons() {
+        createButtons({rows}) {
+            const questionVal = rows.find(elem=>elem.values.includes('question'))
+            const assignmentVal = rows.find(elem=>elem.values.includes('assignment'))
+            const eventVal = rows.find(elem=>elem.values.includes('event'))
             const con = this.createElement('div',{className:'btns-con',id:'btns-con'})
             const mainCon = document.getElementById('main-buttons-block')
             const questionBtn = this.createElement('a',{
-                className:'btn question',
+                className:'btn-primary question',
                 id:'question',
-                textContent: 'Питань'
+                textContent: `Питань: ${questionVal.values[1]}`,
+                dataName:'question'
             })
             const assignmentBtn = this.createElement('a',{
-                className:'btn assignment',
+                className:'btn-primary assignment',
                 id:'assignment',
-                textContent: 'Доручень'
+                textContent: `Доручень: ${assignmentVal.values[1]}`,
+                dataName:'assignment'
             })
             const eventBtn = this.createElement('a',{
-                className:'btn event',
+                className:'btn-primary event',
                 id:'event',
-                textContent: 'Заходів'
+                textContent: `Заходів: ${eventVal.values[1]}`,
+                dataName:'event'
             })
             con.append(questionBtn,assignmentBtn,eventBtn)
+            con.addEventListener('click',this.sendClickData.bind(this))
             mainCon.append(con)
             const title = document.getElementById('title')
             title.remove();
+        },
+        sendClickData(e) {
+            if(e.target.classList.contains('btn-primary')) {
+                let msg = {
+                    name: 'SetButtonData',
+                    package: {
+                        value: e.target.dataName
+                    }
+                };
+                this.messageService.publish(msg);
+            }
         },
         createElement: function(tag, props, ...children) {
             const element = document.createElement(tag);
--- CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/essence_on_control/Widgets/NativeDataGridWidget-0.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/works_pages/essence_on_control/Widgets/NativeDataGridWidget-0.js
@@ -1,8 +1,8 @@
 (function() {
     return {
         config: {
-            /*query: {
-                code: '',
+            query: {
+                code: 'essence_table_SelectRows',
                 parameterValues: [],
                 filterColumns: [],
                 sortColumns: [],
@@ -11,17 +11,132 @@
             },
             columns: [
                 {
-                    dataField: 'Column code',
-                    caption: 'Caption'
+                    dataField: 'RegistrationNumber',
+                    caption: 'Номер'
+                },
+                {
+                    dataField: 'RegistrationDate',
+                    caption: 'Дата реєстрації'
+                },
+                {
+                    dataField: 'TypeName',
+                    caption: 'Тип'
+                },
+                {
+                    dataField: 'StateName',
+                    caption: 'Стан'
+                },
+                {
+                    dataField: 'OrganizationName',
+                    caption: 'Виконавець'
+                },
+                {
+                    dataField: 'ControlDate',
+                    caption: 'Дата контролю'
+                },
+                {
+                    dataField: 'NotificationText',
+                    caption: 'Остання нотифікація'
+                },
+                {
+                    dataField: 'NotificationCreatedAt',
+                    caption: 'Дата відправки'
                 }
             ],
-            keyExpr: 'Id'
+            filterRow: {
+                visible: true,
+                applyFilter: 'auto'
+            },
+            pager: {
+                showPageSizeSelector:  true,
+                allowedPageSizes: [10, 15, 30],
+                showInfo: true
+            },
+            paging: {
+                pageSize: 10
+            },
+            scrolling: {
+                mode: 'standart',
+                rowRenderingMode: null,
+                columnRenderingMode: null,
+                showScrollbar: null
+            },
+            searchPanel: {
+                visible: false,
+                highlightCaseSensitive: true
+            },
+            selection: {
+                mode: 'multiple'
+            },
+            sorting: {
+                mode: 'multiple'
+            },
+            export: {
+                enabled: true,
+                fileName: 'Сутності на контролі'
+            },
+            keyExpr: 'Id',
+            focusedRowEnabled: true,
+            showBorders: false,
+            showColumnLines: false,
+            showRowLines: true,
+            remoteOperations: null,
+            allowColumnReordering: null,
+            rowAlternationEnabled: null,
+            columnAutoWidth: null,
+            hoverStateEnabled: true,
+            columnWidth: null,
+            wordWrapEnabled: true,
+            allowColumnResizing: true,
+            showFilterRow: true,
+            showHeaderFilter: false,
+            showColumnChooser: false,
+            showColumnFixing: true,
+            groupingAutoExpandAll: null
         },
         init: function() {
-            this.loadData(this.afterLoadDataHandler);
+            this.subscribers.push(this.messageService.subscribe('SetButtonData', this.getData, this));
+            this.config.onToolbarPreparing = this.createTableButton.bind(this);
+        },
+        createTableButton(e) {
+            let toolbarItems = e.toolbarOptions.items;
+            toolbarItems.push({
+                widget: 'dxButton',
+                options: {
+                    icon: 'check',
+                    type: 'default',
+                    text: 'Зняти з контролю',
+                    onClick: function(e) {
+                        e.event.stopImmediatePropagation();
+                        this.findAllSelectRowsToArrived();
+                    }.bind(this)
+                },
+                location: 'after'
+            });
+        },
+        findAllSelectRowsToArrived() {
+            let rows = this.dataGridInstance.selectedRowKeys;
+            const rowsValues = rows.map(elem=>`{Id: ${elem}}`)
+            const jsonRows = JSON.stringify(rowsValues)
+            const getButtonsValues = {
+                queryCode: 'essence_table_DeleteRows',
+                limit: -1,
+                parameterValues: [
+                    {key: '@Group', value: this.buttonData},
+                    {key: '@JSON', value: jsonRows}
+                ]
+            };
+            this.queryExecutor(getButtonsValues,this.getNewTable,this);
+        },
+        getNewTable() {
+        },
+        getData(data) {
+            this.buttonData = data.package.value
+            this.config.query.parameterValues = [{key: '@Group', value: data.package.value}, {key: '@UserId', value: '1'}]
+            this.loadData(this.afterLoadDataHandler)
         },
         afterLoadDataHandler: function() {
-            this.render();*/
+            this.render();
         }
     };
 }());
