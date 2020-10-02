9c56ffabef6cfe1875fdbdae4e38e52bb015631d
MarkusEfre <markus.efr@gmail.com>
2020-10-02 16:56:38 +0300

    Task 395 - показ на секции + "Виконавець"

--- CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Claims/list_ExternalExecutors/list_ExternalExecutors.sql
+++ CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Claims/list_ExternalExecutors/list_ExternalExecutors.sql
@@ -3,11 +3,9 @@ DISTINCT
     o.[Id],
     o.[Name]
 FROM
-    dbo.[Organizations] o
-    JOIN dbo.[Contacts] c ON c.Organisation_ID = o.Id
+    [dbo].[Organizations] o
 WHERE
-    c.Contact_type_ID = 2
-    AND o.is_External_service = 1
-    AND #filter_columns#
-        #sort_columns#
-    OFFSET @pageOffsetRows ROWS FETCH next @pageLimitRows ROWS ONLY;
+    o.is_External_service = 1
+   AND #filter_columns#
+       #sort_columns#
+OFFSET @pageOffsetRows ROWS FETCH next @pageLimitRows ROWS ONLY;
