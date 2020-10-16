8de67069dec2bb6f0692e27dfd1032f182c0bbee
MarkusEfre <markus.efr@gmail.com>
2020-10-16 11:37:30 +0300

    399 - внедрил новое поле на места

--- CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Places/avr_PlacesUpdateRow/avr_PlacesUpdateRow.sql
+++ CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Places/avr_PlacesUpdateRow/avr_PlacesUpdateRow.sql
@@ -1,34 +1,48 @@
-IF @place_types_id not in (10,19,6)
+IF @place_types_id NOT IN (10, 19, 6) 
+BEGIN 
+	IF @place_types_id = @place_types_id_ch 
+	BEGIN 
+		RAISERROR (N'Типи співпадають', 16, 1);
+		RETURN;
+	END
+ELSE 
 BEGIN
-	if @place_types_id  = @place_types_id_ch
-	begin
-		print 'Типи совпадають'
-	end
-	 else 
-	begin
-		update Places
-		set
-		  Place_type_ID = isnull(@place_types_id_ch, @place_types_id)
-		 ,Lattitude = @Latitude
-		 ,Longitude = @Longitude
-		 ,District_ID = @distincts_id
-		where Id = @Id
-
-		update Houses
-		set
-		  Longitude = @Longitude
-		 ,Latitude = @Latitude
-		 ,District_id = @distincts_id
-		where Id = (select Street_id from Places where id = @Id)
-	end
+UPDATE
+	[dbo].[Places]
+SET
+	Place_type_ID = isnull(@place_types_id_ch, @place_types_id),
+	Lattitude = @Latitude,
+	Longitude = @Longitude,
+	District_ID = @distincts_id,
+	Comment = @Comment
+WHERE
+	Id = @Id;
+	
+UPDATE
+	[dbo].[Houses]
+SET
+	Longitude = @Longitude,
+	Latitude = @Latitude,
+	District_id = @distincts_id
+WHERE
+	Id = (
+		SELECT
+			Street_id
+		FROM
+			Places
+		WHERE
+			id = @Id
+	);
 END
-
-IF @place_types_id in (10,19,6)
+END 
+IF @place_types_id IN (10, 19, 6) 
 BEGIN
-		update Places
-		set
-		  Lattitude = @Latitude
-		 ,Longitude = @Longitude
-		 ,District_id = @distincts_id
-		where Id = @Id
+UPDATE
+	[dbo].[Places]
+SET
+	Lattitude = @Latitude,
+	Longitude = @Longitude,
+	District_id = @distincts_id
+WHERE
+	Id = @Id;
 END
