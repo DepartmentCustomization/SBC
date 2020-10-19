c59e7e840d6915123b252780f3be6ce76755c533
artemkov1994 <artem_kov@email.ua>
2020-10-20 02:41:24 +0300

    1551_992 изменил время на 11

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/starting_moderator/db_StartMod_Main/db_StartMod_Main.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/starting_moderator/db_StartMod_Main/db_StartMod_Main.sql
@@ -8,9 +8,9 @@
  (
  DATEADD(HOUR,
       DATEDIFF(HOUR,
-         CONVERT(datetime, SWITCHOFFSET(datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 11, 0, 0, 0), DATEPART(TZOFFSET,datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 11, 0, 0, 0) AT TIME ZONE 'E. Europe Standard Time'))),
-         datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 11, 0, 0, 0)
-         ), datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 11, 0, 0, 0))
+         CONVERT(datetime, SWITCHOFFSET(datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 9, 0, 0, 0), DATEPART(TZOFFSET,datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 9, 0, 0, 0) AT TIME ZONE 'E. Europe Standard Time'))),
+         datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 9, 0, 0, 0)
+         ), datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 9, 0, 0, 0))
  )
  --datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 11, 0, 0, 0);
  declare @datetime_to datetime = 
@@ -18,7 +18,7 @@
  (
  DATEADD(HOUR,
       DATEDIFF(HOUR,
-         CONVERT(datetime, SWITCHOFFSET(datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 18, 0, 0, 0), DATEPART(TZOFFSET,datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 11, 0, 0, 0) AT TIME ZONE 'E. Europe Standard Time'))),
+         CONVERT(datetime, SWITCHOFFSET(datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 18, 0, 0, 0), DATEPART(TZOFFSET,datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 18, 0, 0, 0) AT TIME ZONE 'E. Europe Standard Time'))),
          datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 18, 0, 0, 0)
          ), datetimefromparts(year(getutcdate()), month(getutcdate()), day(getutcdate()), 18, 0, 0, 0))
  )
