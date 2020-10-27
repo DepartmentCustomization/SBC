e945bb531457ee14d7124777d282002181dd6f2c
artemkov1994 <artem_kov@email.ua>
2020-10-27 13:36:35 +0200

    1551_918 изменил согласно комментарию

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Statistics_on_appeals_from_site/SAFS_MainTable/SAFS_MainTable.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Statistics_on_appeals_from_site/SAFS_MainTable/SAFS_MainTable.sql
@@ -1,10 +1,19 @@
 
 
 
+
 /*
-declare @date_from date='2019-09-01',
-  @date_to date='2020-09-30';
+declare @date_from datetime='2020-10-01T00:00:00.000Z',
+  @date_to date='2020-10-26T19:53:00.000Z';
   */
+
+  --convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))
+   /*
+  set @date_from=
+    CONVERT(datetime, SWITCHOFFSET(@date_from, DATEPART(TZOFFSET,@date_from AT TIME ZONE 'E. Europe Standard Time')))
+  set @date_to=
+	CONVERT(datetime, SWITCHOFFSET(@date_to, DATEPART(TZOFFSET,@date_to AT TIME ZONE 'E. Europe Standard Time')))
+	*/
 
   declare @reportcode nvarchar(200)=N'appeals_statistics';
 
@@ -16,32 +25,33 @@ declare @date_from date='2019-09-01',
 
   ltrim(convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to)))))) inf2_2,
 
-  (select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=dateadd(dd, -1, getutcdate())) cell2,
+  (select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))=convert(date, getdate())) cell2, --+
+
 
   case 
   when 
-  convert(numeric(8,2),(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=dateadd(dd, -1, getutcdate())))-
-  convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to)))))<=0
+  convert(numeric(8,2),(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))=dateadd(dd, -1, getdate())))-
+  convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to)))))<=0 --+
   then 
-  ltrim(convert(numeric(8,2),(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=dateadd(dd, -1, getutcdate())))-
-  convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to))))))
+  ltrim(convert(numeric(8,2),(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))=dateadd(dd, -1, getdate())))-
+  convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to)))))) --+
   else
-  N'+'+ltrim(convert(numeric(8,2),(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=dateadd(dd, -1, getutcdate())))-
-  convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to))))))
+  N'+'+ltrim(convert(numeric(8,2),(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))=dateadd(dd, -1, getdate())))-
+  convert(numeric(8,2),convert(float,count(Id))/convert(float,1+(datediff(dd, convert(date, @date_from), convert(date, @date_to)))))) --+
   end
-  cell2_delta,
-  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where [AppealFromSiteResultId]=1 and convert(date, ReceiptDate) between convert(date, @date_from) and convert(date, @date_to)))+
-  N'('+ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=getutcdate() and [AppealFromSiteResultId]=1))+
+  cell2_delta,+
+  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where [AppealFromSiteResultId]=1 and convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time')))) between convert(date, @date_from) and convert(date, @date_to)))+
+  N'('+ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))=convert(date, getdate()) and [AppealFromSiteResultId]=1))+
 
-  N'/'+ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)<>getutcdate() and [AppealFromSiteResultId]=1
-  and convert(date, ReceiptDate) between convert(date, @date_from) and convert(date, @date_to)))+N')' cell3,
+  N'/'+ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))<>convert(date, getdate()) and [AppealFromSiteResultId]=1
+  and convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time')))) between convert(date, @date_from) and convert(date, @date_to)))+N')' cell3, --+
 
-  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where [AppealFromSiteResultId]=1 and convert(date, ReceiptDate) between convert(date, @date_from) and convert(date, @date_to))) inf3_1,
+  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where [AppealFromSiteResultId]=1 and convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time')))) between convert(date, @date_from) and convert(date, @date_to))) inf3_1, --+
 
-  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=getutcdate() and [AppealFromSiteResultId]=1)) inf3_2,
+  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))=convert(date, getdate()) and [AppealFromSiteResultId]=1)) inf3_2, --+
 
-  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)<>getutcdate() and [AppealFromSiteResultId]=1
-  and convert(date, ReceiptDate) between convert(date, @date_from) and convert(date, @date_to))) inf3_3,
+  ltrim((select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))<>convert(date, getdate()) and [AppealFromSiteResultId]=1
+  and convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time')))) between convert(date, @date_from) and convert(date, @date_to))) inf3_3, --+
 
   (select sum([indicator_value])
   
@@ -49,7 +59,7 @@ declare @date_from date='2019-09-01',
   where [indicator_name] in (N'Заяввники без телефону та адреси', N'Заявники без адреси, з телефоном')
   and date between convert(date, @date_from) and convert(date, @date_to)) cell4,
 
-  (select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate) between convert(date, @date_from) and convert(date, @date_to) and [AppealFromSiteResultId]=2) cell5
+  (select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time')))) between convert(date, @date_from) and convert(date, @date_to) and [AppealFromSiteResultId]=2) cell5
  /* */
 
 
@@ -61,14 +71,14 @@ declare @date_from date='2019-09-01',
  ,sum(case when charindex(N'android', [SystemIP], 1)>1 then 1 else 0 end) cell4_android
  ,sum(case when charindex(N'Apple', [SystemIP], 1)>1 then 1 else 0 end) cell4_Apple
  --,sum(case when [AppealFromSiteResultId]=2 /*повернуто заявникові*/ then 1 else 0 end) cell5_ReturnToApplicant
- ,(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date,ReceiptDate)=convert(date, getutcdate()) and [AppealFromSiteResultId]=2) cell5_ReturnToApplicant
+ ,(select count(Id) from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite] where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time'))))=convert(date, getdate()) and [AppealFromSiteResultId]=2) cell5_ReturnToApplicant --+
 
 
 
 
  into #temp_main
   from [CRM_1551_Site_Integration].[dbo].[AppealsFromSite]
-  where convert(date, ReceiptDate) between convert(date, @date_from) and convert(date, @date_to)
+  where convert(date, CONVERT(datetime, SWITCHOFFSET(ReceiptDate, DATEPART(TZOFFSET,ReceiptDate AT TIME ZONE 'E. Europe Standard Time')))) between convert(date, @date_from) and convert(date, @date_to) --+
 
   select Id, cell1, cell2, cell2_delta, cell3, cell4, cell5, cell1_count_applicants, cell1_count_applicants_more1appeals
  ,cell1_count_applicants_verified, cell4_windows, cell4_android, cell4_Apple, cell5_ReturnToApplicant
@@ -96,3 +106,4 @@ declare @date_from date='2019-09-01',
   from #temp_main
 
   --df
+
