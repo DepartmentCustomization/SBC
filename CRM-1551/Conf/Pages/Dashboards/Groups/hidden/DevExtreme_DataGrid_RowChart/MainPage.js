18ab5f7435c565d89a1a92350203c8e0d65a61de
Yehor Sinkov <egorkatornado@gmail.com>
2020-10-30 16:03:01 +0200

    add devextreme chart

--- CRM-1551/Conf/Pages/Dashboards/Groups/hidden/DevExtreme_DataGrid_RowChart/MainPage.js
+++ CRM-1551/Conf/Pages/Dashboards/Groups/hidden/DevExtreme_DataGrid_RowChart/MainPage.js
@@ -4,18 +4,16 @@
             let select2LibraryJS = 'select2LibraryJS';
             if (!document.getElementById(select2LibraryJS)) {
                 let head = document.getElementsByTagName('head')[0];
-                let script = document.createElement('script');
-                script.id = 'jQueryLibraryJS';
-                script.type = 'text/javascript';
-                script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
-                head.appendChild(script);
-                script.onload = function() {
-                    let script2 = document.createElement('script');
-                    script2.id = 'devexpress';
-                    script2.type = 'text/javascript';
-                    script2.src = 'https://cdn3.devexpress.com/jslib/20.2.3/js/dx.all.js';
-                    head.appendChild(script2);
-                }.bind(self);
+                let scriptJQuery = document.createElement('script');
+                scriptJQuery.id = 'jQueryLibraryJS';
+                scriptJQuery.type = 'text/javascript';
+                scriptJQuery.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+                head.appendChild(scriptJQuery);
+                let scriptDevExpress = document.createElement('script');
+                scriptDevExpress.id = 'jQueryLibraryJS';
+                scriptDevExpress.type = 'text/javascript';
+                scriptDevExpress.src = 'https://cdn3.devexpress.com/jslib/20.2.3/js/dx.all.js';
+                head.appendChild(scriptDevExpress);
             }
         },
     };
---
+++ CRM-1551/Conf/Pages/Dashboards/Groups/hidden/DevExtreme_DataGrid_RowChart/Widgets/simple_ChartsDevextremeWidget.js
@@ -0,0 +1,41 @@
+(function() {
+    return {
+        config: {
+            query: {
+                code: 'DevExtreme_DataGrid_RowChart_SelectRows',
+                parameterValues: [],
+                filterColumns: [],
+                sortColumns: [],
+                skipNotVisibleColumns: true,
+                chunkSize: 1000
+            },
+            series: {
+                argumentField: "val",
+                valueField: "Name",
+                name: "My oranges",
+                type: "bar",
+                color: '#ffaa66'
+            },
+            "export": {
+                enabled: true
+            },
+            tooltip: {
+                zIndex: 9999,
+                enabled: true,
+                location: "edge",
+                customizeTooltip: function (arg) {
+                    return {
+                        text: "Open: $" + arg.argument + "<br/>" +
+                                "Close: $" + arg.originalValue + "<br/>"
+                    };
+                }
+            }
+        },
+        init: function() {
+            this.loadData(this.afterLoadDataHandler);
+        },
+        afterLoadDataHandler: function() {
+            this.render();
+        }
+    };
+}());
