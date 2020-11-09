cf1cc9e3aae3204d9b7c782459420d024e366aa2
artemkov1994 <artem_kov@email.ua>
2020-11-09 20:06:07 +0200

    1551_1022

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/API_mobile_1551/mobile_QuestionTypes/mobile_QuestionTypes.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/API_mobile_1551/mobile_QuestionTypes/mobile_QuestionTypes.sql
@@ -1,9 +1,11 @@
 SELECT 
-	[Id],
-	[Name],
-	[question_type_id] AS [parentId],
-	[Index] 
-FROM dbo.[QuestionTypes] 
+	qt.[Id],
+	qt.[Name],
+	qt.[question_type_id] AS [parentId],
+	pqt.[name] AS [parentName],
+	qt.[Index] 
+FROM dbo.[QuestionTypes] qt
+left join dbo.[QuestionTypes] pqt on qt.question_type_id=pqt.Id
 WHERE 
 	#filter_columns#
 	#sort_columns#
