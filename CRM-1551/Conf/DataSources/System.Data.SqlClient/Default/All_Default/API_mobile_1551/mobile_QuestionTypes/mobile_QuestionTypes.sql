a572a49f2f12a2aed8cb82959b5d6c6282ed99ba
MarkusEfre <markus.efr@gmail.com>
2020-11-25 12:34:20 +0200

    1028 - правки моб. API 25.11.20

--- CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/API_mobile_1551/mobile_QuestionTypes/mobile_QuestionTypes.sql
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/API_mobile_1551/mobile_QuestionTypes/mobile_QuestionTypes.sql
@@ -1,12 +1,18 @@
-SELECT 
+-- DECLARE @Id INT = NULL,
+-- 		@Name NVARCHAR(500);
+
+SELECT
 	qt.[Id],
 	qt.[Name],
 	qt.[question_type_id] AS [parentId],
 	pqt.[name] AS [parentName],
-	qt.[Index] 
-FROM dbo.[QuestionTypes] qt
-left join dbo.[QuestionTypes] pqt on qt.question_type_id=pqt.Id
-WHERE 
-	#filter_columns#
+	qt.[Index]
+FROM
+	dbo.[QuestionTypes] qt
+	LEFT JOIN dbo.[QuestionTypes] pqt ON qt.question_type_id = pqt.Id
+WHERE
+	qt.Id = IIF(@Id IS NULL, qt.Id, @Id)
+AND qt.[name] = IIF(@Name IS NULL, qt.[name], @Name) 
+AND #filter_columns#
 	#sort_columns#
 OFFSET @pageOffsetRows ROWS FETCH NEXT @pageLimitRows ROWS ONLY;
