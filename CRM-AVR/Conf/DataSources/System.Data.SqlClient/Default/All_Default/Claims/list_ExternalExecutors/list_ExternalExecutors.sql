d908456b6f82776c2bccf03a28cfb840d63e0a76
MarkusEfre <markus.efr@gmail.com>
2020-11-27 15:50:13 +0200

    419 - "Виконавець" по is_Contract_organization = 1

--- CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Claims/list_ExternalExecutors/list_ExternalExecutors.sql
+++ CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Claims/list_ExternalExecutors/list_ExternalExecutors.sql
@@ -1,14 +1,11 @@
 SELECT
 DISTINCT
-    o.[Id],
-    o.[Name],
-	c.Contact_type_ID
-FROM
+    [Id],
+    [Name]
+FROM 
     dbo.[Organizations] o
-    INNER JOIN dbo.[Contacts] c ON c.Organisation_ID = o.Id
 WHERE
-    o.is_External_service = 1
-	AND c.Contact_type_ID = 5
-	AND #filter_columns#
-        #sort_columns#
+    is_Contract_organization = 1
+AND #filter_columns#
+    #sort_columns#
 OFFSET @pageOffsetRows ROWS FETCH next @pageLimitRows ROWS ONLY;
