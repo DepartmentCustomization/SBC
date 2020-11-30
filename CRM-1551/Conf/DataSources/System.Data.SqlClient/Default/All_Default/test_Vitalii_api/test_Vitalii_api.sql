26e780d0bdbdb8792675fd3dcdbc888de04e736b
artemkov1994 <artem_kov@email.ua>
2020-11-30 17:50:45 +0200

    test_Vitalii_api

---
+++ CRM-1551/Conf/DataSources/System.Data.SqlClient/Default/All_Default/test_Vitalii_api/test_Vitalii_api.sql
@@ -0,0 +1,156 @@
+
+
+
+select 1 Id,
+
+N'
+{
+                    about:{
+                        category:''Авторизація'',
+                        description:''Вулиці''
+                    },
+                    request: {
+                        url:''http://apps.ukrods.cf:8075/CRM1551/api/section/API_web_1551_Applicant  '',
+                        method: ''PUT'',
+                        headers: {
+                            Authorization: ''Bearer 9S8oX3rhjibX_-rhji…'',
+                            ContentType: ''application/json''
+                        },
+                        body:''JSON''
+                    },
+                    response: {
+                        result:''JSON'',
+                        error: ''error number''
+                    }
+                }
+' api_cod
+
+union all
+
+select 2 Id,
+
+N'
+{
+                    about:{
+                        category:''Авторизація'',
+                        description:''Вулиці''
+                    },
+                    request: {
+                        url:''http://apps.ukrods.cf:8075/CRM1551/api/section/API_web_1551_Applicant  '',
+                        method: ''DELETE'',
+                        headers: {
+                            Authorization: ''Bearer 9S8oX3rhjibX_-rhji…'',
+                            ContentType: ''application/json''
+                        },
+                        body:''JSON''
+                    },
+                    response: {
+                        result:''JSON'',
+                        error: ''error number''
+                    }
+                }
+' api_cod
+
+union all
+
+select 3 Id,
+
+N'
+{
+                    about:{
+                        category:''Авторизація'',
+                        description:''Вулиці''
+                    },
+                    request: {
+                        url:''http://apps.ukrods.cf:8075/CRM1551/api/section/API_web_1551_Applicant  '',
+                        method: ''POST'',
+                        headers: {
+                            Authorization: ''Bearer 9S8oX3rhjibX_-rhji…'',
+                            ContentType: ''application/json''
+                        },
+                        body:''JSON''
+                    },
+                    response: {
+                        result:''JSON'',
+                        error: ''error number''
+                    }
+                }
+' api_cod
+
+union all
+
+select 4 Id,
+
+N'
+{
+                    about:{
+                        category:''Довідники'',
+                        description:''Будинки''
+                    },
+                    request: {
+                        url:''http://apps.ukrods.cf:8075/CRM1551/api/section/API_web_1551_Applicant  '',
+                        method: ''POST'',
+                        headers: {
+                            Authorization: ''Bearer 9S8oX3rhjibX_-rhji…'',
+                            ContentType: ''application/json''
+                        },
+                        body:''JSON''
+                    },
+                    response: {
+                        result:''JSON'',
+                        error: ''error number''
+                    }
+                }
+' api_cod
+
+union all
+select 5 Id,
+
+N'
+{
+                    about:{
+                        category:''Довідники'',
+                        description:''Організації''
+                    },
+                    request: {
+                        url:''http://apps.ukrods.cf:8075/CRM1551/api/section/API_web_1551_Applicant  '',
+                        method: ''PUT'',
+                        headers: {
+                            Authorization: ''Bearer 9S8oX3rhjibX_-rhji…'',
+                            ContentType: ''application/json''
+                        },
+                        body:''JSON''
+                    },
+                    response: {
+                        result:''JSON'',
+                        error: ''error number''
+                    }
+                }
+' api_cod
+
+union all
+
+select 6 Id,
+
+N'
+{
+                    about:{
+                        category:''Отримати список задач'',
+                        description:''Вулиці''
+                    },
+                    request: {
+                        url:''http://apps.ukrods.cf:8075/CRM1551/api/section/API_web_1551_Applicant  '',
+                        method: ''DELETE'',
+                        headers: {
+                            Authorization: ''Bearer 9S8oX3rhjibX_-rhji…'',
+                            ContentType: ''application/json''
+                        },
+                        body:''JSON''
+                    },
+                    response: {
+                        result:''JSON'',
+                        error: ''error number''
+                    }
+                }
+' api_cod
+
