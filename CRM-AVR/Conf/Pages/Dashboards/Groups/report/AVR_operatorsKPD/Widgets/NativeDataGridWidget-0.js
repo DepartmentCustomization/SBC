7d97792785c9a461bfe46237d38f1d65544a3e4f
Виталий Стародуб <v.starodub@domain.local>
2020-12-22 17:48:22 +0200

    add new logic

---
+++ CRM-AVR/Conf/Pages/Dashboards/Groups/report/AVR_operatorsKPD/Widgets/NativeDataGridWidget-0.js
@@ -0,0 +1,141 @@
+(function() {
+    return {
+        config: {
+            query: {
+                code: 'db_kpd_MainTable',
+                parameterValues: [],
+                filterColumns: [],
+                sortColumns: [],
+                skipNotVisibleColumns: true,
+                chunkSize: 1000
+            },
+            columns: [
+                {
+                    dataField: 'date',
+                    caption: 'Дата',
+                    alignment:'center',
+                    width:'200'
+                }, {
+                    dataField: 'PIB_operator',
+                    caption: 'ПІБ оператора',
+                    alignment:'center',
+                    width:'300'
+                }, {
+                    dataField: 'subdivision',
+                    caption: 'Підрозділ',
+                    alignment:'center',
+                    width:'250'
+                }, {
+                    dataField: 'count_open_claims',
+                    caption: 'Відкрито заявок',
+                    alignment:'center'
+                }, {
+                    dataField: 'count_close_claims',
+                    caption: 'Закрито заявок',
+                    alignment:'center'
+                }, {
+                    dataField: 'count_all_claims',
+                    caption: 'ВСЬОГО',
+                    alignment:'center',
+                    width:'200'
+                }, {
+                    dataField: 'count_open_orders',
+                    caption: 'Відкрито виїздів',
+                    alignment:'center'
+                }, {
+                    dataField: 'count_close_orders',
+                    caption: 'Закрито виїздів',
+                    alignment:'center'
+                }, {
+                    dataField: 'count_all_orders',
+                    caption: 'ВСЬОГО',
+                    alignment:'center',
+                    width:'200'
+                }
+            ],
+            keyExpr: 'Id',
+            scrolling: {
+                mode: 'virtual'
+            },
+            filterRow: {
+                visible: true,
+                applyFilter: 'auto'
+            },
+            showBorders: true,
+            showColumnLines: true,
+            showRowLines: true,
+            remoteOperations: null,
+            allowColumnReordering: null,
+            rowAlternationEnabled: null,
+            columnAutoWidth: null,
+            hoverStateEnabled: true,
+            columnWidth: 'auto',
+            wordWrapEnabled: true,
+            allowColumnResizing: true,
+            showFilterRow: true,
+            showHeaderFilter: true,
+            showColumnChooser: false,
+            showColumnFixing: true,
+            groupingAutoExpandAll: null,
+            export: {
+                enabled: true,
+                fileName: 'Відомість перекритих засувок'
+            }
+        },
+        init: function() {
+            this.sub = this.messageService.subscribe('GlobalFilterChanged', this.getFiltersParams, this);
+            this.dataGridInstance.height = window.innerHeight - 150;
+        },
+        showTopQuestionsTable: function() {
+            document.getElementById('cars_report').style.display = 'block';
+        },
+        changeDateTimeValues: function(value) {
+            let trueDate;
+            if (value !== null) {
+                let date = new Date(value);
+                let dd = date.getDate();
+                let MM = date.getMonth();
+                let yyyy = date.getFullYear();
+                let HH = date.getUTCHours()
+                let mm = date.getMinutes();
+                MM += 1;
+                if ((dd.toString()).length === 1) {
+                    dd = '0' + dd;
+                }
+                if ((MM.toString()).length === 1) {
+                    MM = '0' + MM;
+                }
+                if ((HH.toString()).length === 1) {
+                    HH = '0' + HH;
+                }
+                if ((mm.toString()).length === 1) {
+                    mm = '0' + mm;
+                }
+                trueDate = dd + '.' + MM + '.' + yyyy;
+            } else {
+                trueDate = ' ';
+            }
+            return trueDate;
+        },
+        getFiltersParams: function(message) {
+            let period = message.package.value.values.find(f => f.name === 'period').value;
+            if (period !== null) {
+                if (period.dateFrom !== '' && period.dateTo !== '') {
+                    this.dateFrom = period.dateFrom;
+                    this.dateTo = period.dateTo;
+                    this.config.query.parameterValues = [
+                        { key: '@dateFrom', value: this.dateFrom },
+                        { key: '@dateTo', value: this.dateTo }
+                    ];
+                    this.loadData(this.afterLoadDataHandler);
+                }
+            }
+        },
+        afterLoadDataHandler: function() {
+            this.render();
+        },
+        destroy: function() {
+            this.sub.unsubscribe();
+        }
+    };
+}());
