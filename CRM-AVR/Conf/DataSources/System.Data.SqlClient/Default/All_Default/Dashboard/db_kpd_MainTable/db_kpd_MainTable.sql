75b8fff5ff4bfc34b354f53b14c989f3e51bcd34
artemkov1994 <artem_kov@email.ua>
2020-12-23 21:42:50 +0200

    avr415 залил основной запрос

--- CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Dashboard/db_kpd_MainTable/db_kpd_MainTable.sql
+++ CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Dashboard/db_kpd_MainTable/db_kpd_MainTable.sql
@@ -1,21 +1,84 @@
--- declare @date_from datetime='2020-10-06 16:21:26.830',
---   @date_to datetime=getutcdate()
+/*
+declare @date_from datetime='2020-11-26 08:34:32.563',
+	@date_to datetime='2020-12-30 08:34:32.563';
+*/
+  if object_id('tempdb..#temp_claims_open') is not NULL drop table #temp_claims_open
 
-  select 1 Id, convert(date, @date_from) date, N'Вася' PIB_operator, N'важное' subdivision, 
-  100 count_open_claims, 100 count_close_claims, 200 count_all_claims,
-  50 count_open_orders, 500 count_close_orders, 550 count_all_orders union all
-  select 2 Id, convert(date, @date_from) date, N'Федя' PIB_operator, N'неважное' subdivision, 
-  20 count_open_claims, 200 count_close_claims, 220 count_all_claims,
-  50 count_open_orders, 100 count_close_orders, 150 count_all_orders union all
-  select 3 Id, convert(date, @date_to) date, N'Степа' PIB_operator, N'важное' subdivision, 
-  100 count_open_claims, 100 count_close_claims, 200 count_all_claims,
-  50 count_open_orders, 100 count_close_orders, 150 count_all_orders union all
-  select 4 Id, convert(date, @date_to) date, N'Петя' PIB_operator, N'неважное' subdivision, 
-  1000 count_open_claims, 100 count_close_claims, 1100 count_all_claims,
-  500 count_open_orders, 100 count_close_orders, 600 count_all_orders union all
-  select 5 Id, convert(date, '2020-12-12 12:10:02') date, N'Тест' PIB_operator, N'неважное' subdivision, 
-  1000 count_open_claims, 100 count_close_claims, 1100 count_all_claims,
-  500 count_open_orders, 100 count_close_orders, 600 count_all_orders union all
-  select 6 Id, convert(date, '2020-11-12 11:10:02') date, N'Тест2' PIB_operator, N'важное' subdivision, 
-  1000 count_open_claims, 100 count_close_claims, 1100 count_all_claims,
-  500 count_open_orders, 100 count_close_orders, 600 count_all_orders
+  select [User], convert(date, [Created_at]) [date], count(Id) count_claims_open
+  into #temp_claims_open
+  from [dbo].[Claims]
+  where [Created_at] between @date_from and @date_to
+  group by [User], convert(date, [Created_at])
+
+  if object_id('tempdb..#temp_claims_close') is not NULL drop table #temp_claims_close
+
+  select [User], convert(date, [Fact_finish_at]) [date], count(Id) count_claims_close
+  into #temp_claims_close
+  from [dbo].[Claims]
+  where [Fact_finish_at] between @date_from and @date_to
+  group by [User], convert(date, [Fact_finish_at])
+
+  if object_id('tempdb..#temp_orders_open') is not NULL drop table #temp_orders_open
+
+  select [User_id] [User], convert(date,[Created_at]) [date], count(Id) count_orders_open
+  into #temp_orders_open
+  from [dbo].[Orders]
+  where [Created_at] between @date_from and @date_to
+  group by [User_id], convert(date,[Created_at])
+
+  if object_id('tempdb..#temp_orders_close') is not NULL drop table #temp_orders_close
+
+  select [User_id] [User], convert(date,[Closed_at]) [date], count(Id) count_orders_close
+  into #temp_orders_close
+  from [dbo].[Orders]
+  where [Closed_at] between @date_from and @date_to
+  group by [User_id], convert(date,[Closed_at])
+
+
+  if object_id('tempdb..#temp_main') is not NULL drop table #temp_main
+  --create table #temp_main ([User] nvarchar(128), [date] date)
+
+  select [User], [date]
+  into #temp_main
+  from
+  (
+  select [User], date
+  from #temp_claims_open
+	union
+  select [User], date
+  from #temp_claims_close
+	union
+  select [User], date
+  from #temp_orders_open
+	union
+  select [User], date
+  from #temp_orders_close
+  ) t
+
+
+
+  --select * from #temp_orders_open
+
+
+  select ROW_NUMBER() over(order by tm.date, o.Short_name) Id,
+  tm.date, isnull(u.LastName+N' ', N'')+isnull(u.FirstName,N'') PIB_operator, o.Short_name subdivision, 
+  isnull(tco.count_claims_open,0) count_open_claims, 
+  isnull(tcc.count_claims_close,0) count_close_claims, 
+  isnull(tco.count_claims_open,0)+isnull(tcc.count_claims_close,0) count_all_claims,
+
+  isnull(too.count_orders_open,0) count_open_orders,
+  isnull(toc.count_orders_close,0) count_close_orders,
+  isnull(too.count_orders_open,0)+isnull(toc.count_orders_close,0) count_all_orders
+
+  from #temp_main tm
+  left join [dbo].[SysUser_OrgWC] so on tm.[User]=so.SystemUser_Id
+  left join [dbo].[Organizations] o on so.OrganizationWC_Id=o.Id
+  left join [CRM_AVR_System].[dbo].[User] u on so.SystemUser_Id=u.UserId
+  left join #temp_claims_open tco on tm.[User]=tco.[User] and tm.date=tco.date
+  left join #temp_claims_close tcc on tm.[User]=tcc.[User] and tm.date=tcc.date
+  left join #temp_orders_open too on tm.[User]=too.[User] and tm.date=too.date
+  left join #temp_orders_close toc on tm.[User]=toc.[User] and tm.date=toc.date
+--   where 
+--   #filter_columns#
+--   #sort_columns#
+--  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
