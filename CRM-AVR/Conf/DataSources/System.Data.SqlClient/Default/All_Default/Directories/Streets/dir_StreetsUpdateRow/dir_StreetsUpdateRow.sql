e02c72707416e8830a56fb5bb1d87b7bbc2e12a1
artemkov1994 <artem_kov@email.ua>
2020-12-29 14:57:11 +0200

    avr428 вывод сообщения

--- CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Directories/Streets/dir_StreetsUpdateRow/dir_StreetsUpdateRow.sql
+++ CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Directories/Streets/dir_StreetsUpdateRow/dir_StreetsUpdateRow.sql
@@ -1,9 +1,18 @@
-UPDATE
-   [dbo].[Streets]
-SET
-   [Street_type_id] = @type_id,
-   [Name] = @Name,
-   [Old_name] = @Old_name,
-   [Territory] = @Territory
-WHERE
-   Id = @Id;
+IF(select isnull(ltrim([Street_type_id]), N'')+isnull(Name, N'') from [dbo].[Streets] s where s.Id=@Id)=isnull(ltrim(@type_id), N'')+isnull(@Name, N'')
+	  BEGIN
+		RAISERROR(N'Слід коректно ввести назву вулиці', 16, 1);
+		RETURN;
+  END
+
+ELSE
+	BEGIN
+		UPDATE
+		   [dbo].[Streets]
+		SET
+		   [Street_type_id] = @type_id,
+		   [Name] = @Name,
+		   [Old_name] = @Old_name,
+		   [Territory] = @Territory
+		WHERE
+		   Id = @Id;
+	END
