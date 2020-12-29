889898024df8be54f51683702ce27b41ccf7a422
artemkov1994 <artem_kov@email.ua>
2020-12-29 23:08:40 +0200

    avr434

--- CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Dashboard/AVR_closedFaucets/AVR_SelectClosedFaucets.sql
+++ CRM-AVR/Conf/DataSources/System.Data.SqlClient/Default/All_Default/Dashboard/AVR_closedFaucets/AVR_SelectClosedFaucets.sql
@@ -1,7 +1,30 @@
+
 /*
-DECLARE @dateFrom DATETIME = '2020-08-01 00:00:00',
+DECLARE @dateFrom DATETIME = '2020-11-30T22:00:00.000Z',
 		@dateTo DATETIME = GETDATE();
 */
+--обработка мест по засувкам новое начало
+
+IF OBJECT_ID('tempdb..#claims_faucetClosedPlace2') IS NOT NULL DROP TABLE #claims_faucetClosedPlace2
+
+SELECT [Faucet].Id Faucet_Id, [Faucet].Claim_Id, Places.Name places_name, 
+
+CONVERT(datetime, SWITCHOFFSET([Faucet].Start_from, DATEPART(TZOFFSET,[Faucet].Start_from AT TIME ZONE 'E. Europe Standard Time'))) Start_from,
+
+--[Faucet].Start_from, 
+
+CONVERT(datetime, SWITCHOFFSET([Faucet].Finish_at, DATEPART(TZOFFSET,[Faucet].Finish_at AT TIME ZONE 'E. Europe Standard Time'))) Finish_at,
+
+--[Faucet].Finish_at,
+
+	Diameters.Size diameters_size
+into #claims_faucetClosedPlace2
+FROM [dbo].[Faucet]
+	left join Diameters on Diameters.Id = Faucet.Diametr_Id
+	left join Places on Places.Id = Faucet.Place_Id
+WHERE [Faucet].Start_from BETWEEN @dateFrom AND @dateTo
+--обработка мест по засувкам новое конец
+/*
 
 ---> обработка мест по засувкам
 IF OBJECT_ID('tempdb..#claims_faucetClosedPlace') IS NOT NULL
@@ -174,65 +197,87 @@ WHERE Id = @current_id;
 END
 
 DECLARE @targettimezone AS sysname = 'E. Europe Standard Time';
-
+*/
 SELECT 
 	c.Id,
 	c.Claim_Number,
 	o.Short_name AS orgName,
 	p_main_place.[Name] AS claim_place,
 	ct.[Full_Name] AS claim_type,
-	faucet_diameter = ISNULL(ltrim(stuff((SELECT ', '+ fcp.faucet_size
-           FROM #claims_faucetClosedPlace fcp
-		   INNER JOIN dbo.Claims c1 ON c1.Id = fcp.claimId
-		   WHERE c1.Id = c.Id
-		   GROUP BY fcp.faucet_size
-		   ORDER BY fcp.faucet_size	
-           FOR XML PATH('')), 1, 1, '')), N'Не вказано'),
+	--faucet_diameter = ISNULL(ltrim(stuff((SELECT ', '+ fcp.faucet_size
+ --          FROM #claims_faucetClosedPlace fcp
+	--	   INNER JOIN dbo.Claims c1 ON c1.Id = fcp.claimId
+	--	   WHERE c1.Id = c.Id
+	--	   GROUP BY fcp.faucet_size
+	--	   ORDER BY fcp.faucet_size	
+ --          FOR XML PATH('')), 1, 1, '')), N'Не вказано'),
 
-	faucet_closed_place = ltrim(stuff((SELECT ', '+ fcp.[placeName]
-           FROM #claims_faucetClosedPlace fcp
-		   INNER JOIN dbo.Claims c1 ON c1.Id = fcp.claimId
-		   WHERE c1.Id = c.Id
-		   ORDER BY fcp.placeId
-           FOR XML PATH('')), 1, 1, '')),
+ isnull(ltrim(cfcp.diameters_size),N'Не вказано') faucet_diameter,
 
-	faucet_switchOff_place = ISNULL(ltrim(stuff((SELECT ', '+ f_swOff_p.[placeName]
-           FROM #claims_faucetSwitchOffPlace f_swOff_p
-		   INNER JOIN dbo.Claims c1 ON c1.Id = f_swOff_p.claimId
-		   WHERE c1.Id = c.Id
-		   ORDER BY f_swOff_p.placeId
-           FOR XML PATH('')), 1, 1, '')), N'Всі з водою'),
+	--faucet_closed_place = ltrim(stuff((SELECT ', '+ fcp.[placeName]
+ --          FROM #claims_faucetClosedPlace fcp
+	--	   INNER JOIN dbo.Claims c1 ON c1.Id = fcp.claimId
+	--	   WHERE c1.Id = c.Id
+	--	   ORDER BY fcp.placeId
+ --          FOR XML PATH('')), 1, 1, '')),
+	cfcp.places_name faucet_closed_place,
+
+	--faucet_switchOff_place = ISNULL(ltrim(stuff((SELECT ', '+ f_swOff_p.[placeName]
+ --          FROM #claims_faucetSwitchOffPlace f_swOff_p
+	--	   INNER JOIN dbo.Claims c1 ON c1.Id = f_swOff_p.claimId
+	--	   WHERE c1.Id = c.Id
+	--	   ORDER BY f_swOff_p.placeId
+ --          FOR XML PATH('')), 1, 1, '')), N'Всі з водою'),
+
+	isnull(stuff((
+  select N', '+isnull(p.Name, N'')
+  from [dbo].[Claim_SwitchOff_Address] cs
+  inner join [dbo].[Places] p on cs.Place_ID=p.Id
+  where cs.[Claim_ID]=cfcp.Claim_Id and cs.[Faucet_ID]=cfcp.Faucet_Id
+  for xml path('')),1,2,N''), N'Всі з водою') faucet_switchOff_place,
+
 	--> convert в текст нужного формата с учетом utc
-	CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Start_from) 
-	AT TIME ZONE @targettimezone), MAX(f.Start_from)), 4) + SPACE(1) + 
-	CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Start_from) 
-	AT TIME ZONE @targettimezone), MAX(f.Start_from)), 8)
-		AS faucet_closedStart,
-	CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Finish_at) 
-	AT TIME ZONE @targettimezone), MAX(f.Finish_at)), 4) + SPACE(1) + 
-	CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Finish_at) 
-	AT TIME ZONE @targettimezone), MAX(f.Finish_at)), 8)
-		AS faucet_closedFinish,
+	--CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Start_from) 
+	--AT TIME ZONE @targettimezone), MAX(f.Start_from)), 4) + SPACE(1) + 
+	--CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Start_from) 
+	--AT TIME ZONE @targettimezone), MAX(f.Start_from)), 8)
+	--	AS faucet_closedStart,
+	--ltrim(cfcp.Start_from) faucet_closedStart,
+	format(cfcp.Start_from, 'dd.MM.yy HH:mm:ss') faucet_closedStart,
+	--CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Finish_at) 
+	--AT TIME ZONE @targettimezone), MAX(f.Finish_at)), 4) + SPACE(1) + 
+	--CONVERT(VARCHAR, dateadd(MINUTE,datepart(tz, MAX(f.Finish_at) 
+	--AT TIME ZONE @targettimezone), MAX(f.Finish_at)), 8)
+	--	AS faucet_closedFinish,
+	--ltrim(cfcp.Finish_at) faucet_closedFinish,
+	format(cfcp.Finish_at, 'dd.MM.yy HH:mm:ss') faucet_closedFinish,
 	s.[Name] AS claim_status
 FROM dbo.[Claims] c
-INNER JOIN #claims_faucetClosedPlace cfcp ON cfcp.claimId = c.Id
-INNER JOIN dbo.[Faucet] f ON f.Claim_Id = c.Id 
-LEFT JOIN dbo.[Action_types] [at] ON [at].Id = f.Action_types_Id
+--INNER JOIN #claims_faucetClosedPlace cfcp ON cfcp.claimId = c.Id
+inner join #claims_faucetClosedPlace2 cfcp on c.Id=cfcp.Claim_Id
+--INNER JOIN dbo.[Faucet] f ON f.Claim_Id = c.Id 
+LEFT JOIN dbo.[Action_types] [at] ON [at].Id = cfcp.Faucet_Id
 INNER JOIN dbo.[Organizations] o ON o.Id = c.Response_organization_ID
 INNER JOIN dbo.[Claim_Order_Places] cop_main_place ON cop_main_place.Claim_ID = c.Id 
 	AND cop_main_place.Is_first_place = 1
 INNER JOIN dbo.[Places] p_main_place ON p_main_place.Id = cop_main_place.Place_ID 
 INNER JOIN dbo.[Claim_types] ct ON ct.Id = c.Claim_type_ID   
 INNER JOIN dbo.[Status] s ON s.Id = c.Status_ID
-WHERE f.Start_from  
-BETWEEN @dateFrom AND @dateTo 
-GROUP BY c.Id,
-		 c.Claim_Number,
-		 o.Short_name,
-		 p_main_place.[Name],
-		 ct.[Full_Name],
-		 s.[Name]
+--WHERE f.Start_from  
+--BETWEEN @dateFrom AND @dateTo 
+--GROUP BY c.Id,
+--		 c.Claim_Number,
+--		 o.Short_name,
+--		 p_main_place.[Name],
+--		 ct.[Full_Name],
+--		 s.[Name],
+--		 isnull(ltrim(cfcp.diameters_size),N'Не вказано'),
+--		 cfcp.places_name,
+--		 cfcp.Start_from,
+--		 cfcp.Finish_at
 -- AND #filter_columns#
 ORDER BY 1
 -- OFFSET @pageOffsetRows ROWS FETCH NEXT @pageLimitRows ROWS ONLY
 	;
+
+	--select * from #claims_faucetClosedPlace2
