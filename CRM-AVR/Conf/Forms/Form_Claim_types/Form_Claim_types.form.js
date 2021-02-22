21fb315a3e4f13bb890cfb76e1a4e62265a13f91
artemkov1994 <artem_kov@email.ua>
2021-02-22 15:25:41 +0200

    455 все_л

--- CRM-AVR/Conf/Forms/Form_Claim_types/Form_Claim_types.form.js
+++ CRM-AVR/Conf/Forms/Form_Claim_types/Form_Claim_types.form.js
@@ -17,14 +17,10 @@
             }
             this.form.onControlValueChanged('access_id', this.onType);
             this.form.onControlValueChanged('claim_types_name', this.fullName)
-        }
-        //начало
-        ,afterSave: function(data) {
+        },afterSave: function(data) {
             if (this.form.getControlValue('Is_delete')===true) {
                 this.openPopUpInfoDialog('Цей тип буде видалено, та більше не доступний для використання й редагування');
-            }
-                                }
-        //конец
+            }}
         ,
         fullName: function(name) {
             if(name) {
